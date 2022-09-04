6883c8883753213d6d94a1b37a92441e05db5984
ReoHakase <reonaldhkt+webs@gmail.com>
2022-09-04 19:42:48 +0900

    feat: ✨ (src/components/Button/index.tsx, Button.story.tsx, src/styles/storybook.scss, package.json) Buttonコンポーネントの実装とストーリーの追加
    - Buttonコンポーネントを任意のアイコンとテキストを受け取れるようにHTMLの<button>を拡張する形で実装した。
    - ボタンにアニメーションを実装するためにパッケージ`framer-motion`を導入した。
    くそでかコミットでごめんなさい

--- src/components/Button/Button.story.tsx
+++ src/components/Button/Button.story.tsx
@@ -1,18 +1,74 @@
-import type { ComponentStory, ComponentMeta } from '@storybook/react';
+import type { ComponentStoryObj, ComponentMeta } from '@storybook/react';
 import React from 'react';
 
-import Button from './index';
+import { MdAccessibilityNew } from 'react-icons/md';
+import { Button, ButtonIcon } from './index';
 
-export default {
-  title: 'Button',
+type Story = ComponentStoryObj<typeof Button>;
+
+const meta: ComponentMeta<typeof Button> = {
   component: Button,
-} as ComponentMeta<typeof Button>;
-
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
-
-export const Opaque = Template.bind({});
-Opaque.args = {
-  text: 'ボタン',
-  onClick: () => {},
+  args: {
+    onClick: () => {},
+  },
+  argTypes: {
+    type: {
+      options: ['button', 'submit'],
+      control: { type: 'radio' },
+    },
+    disabled: {
+      control: { type: 'boolean' },
+    },
+    outlined: {
+      control: { type: 'boolean' },
+    },
+  },
 };
-Opaque.storyName = '塗りつぶし型の見た目のボタン';
+
+export default meta;
+
+export const Default: Story = {
+  args: {
+    type: 'button',
+    children: 'デフォルトボタン',
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    type: 'button',
+    disabled: true,
+    children: '無効化されたボタン',
+  },
+};
+
+export const Outlined: Story = {
+  args: {
+    type: 'button',
+    outlined: true,
+    children: '枠線付きボタン',
+  },
+};
+
+export const StyleOverriden: Story = {
+  args: {
+    type: 'button',
+    className: 'bg-gradient-to-br gradient-primary',
+    children: 'スタイルづけされたボタン',
+  },
+};
+
+export const WithReactIcon: Story = {
+  args: {
+    type: 'button',
+    className: 'bg-success-700',
+  },
+  render: (args) => (
+    <Button {...args}>
+      <ButtonIcon>
+        <MdAccessibilityNew />
+      </ButtonIcon>
+      <i>react-icon</i>を使ったボタン
+    </Button>
+  ),
+};
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,24 +1,69 @@
-import Image from 'next/image';
-import type { FC } from 'react';
+import { motion, MotionProps } from 'framer-motion';
+import { FC, ReactNode, ComponentPropsWithoutRef, useMemo } from 'react';
+import { IconContext } from 'react-icons';
 
-type Props = {
-  text?: string;
-  iconUrl?: string;
-  className?: string;
-  onClick: () => void;
+export type ButtonProps = ComponentPropsWithoutRef<'button'> &
+  MotionProps & {
+    type: 'button' | 'submit'; // a11yに従いtypeを要求する。'reset'はformの値を初期化する動作を与えてしまうから使わない。
+    outlined?: boolean; // 簡易にスタイルを切り替えるためのフラグ
+  };
+
+const Button: FC<ButtonProps> = ({ type, outlined, className, children, ...props }) => {
+  //
+  // https://github.com/jsx-eslint/eslint-plugin-react/issues/1555
+  // react/button-has-type は<button>のtype属性の動的割当を禁止しているが、<button>を拡張する際でもアクセシビリティを考慮しtype属性を割り当てるべき。
+  // だから、ユニオン型と条件分岐で回避する
+  //
+  switch (type) {
+    case 'button':
+      return (
+        <motion.button
+          whileHover={{
+            scale: props.disabled ? 1.0 : 1.05,
+            transition: { duration: 0.25 },
+          }}
+          whileTap={{ scale: props.disabled ? 1.0 : 0.95 }}
+          type="button"
+          className={`flex flex-row items-center justify-center gap-2 rounded-3xl px-4 py-2 font-branding text-base font-bold shadow-lg hover:shadow-xl active:shadow-none disabled:shadow-none disabled:contrast-50 ${
+            outlined ? `border-2 border-neutral-200 bg-white text-neutral-900` : ` bg-neutral-900 text-white`
+          } ${className}`}
+          {...props}
+        >
+          {children}
+        </motion.button>
+      );
+    case 'submit':
+    default:
+      return (
+        <motion.button
+          whileHover={{
+            scale: props.disabled ? 1.0 : 1.05,
+            transition: { duration: 0.25 },
+          }}
+          whileTap={{ scale: props.disabled ? 1.0 : 0.95 }}
+          type="submit"
+          className={`flex flex-row items-center justify-center gap-2 rounded-3xl px-4 py-2 font-branding text-base font-bold shadow-lg hover:shadow-xl active:shadow-none disabled:shadow-none disabled:contrast-50 ${
+            outlined ? `border-2 border-neutral-200 bg-white text-neutral-900` : ` bg-neutral-900 text-white`
+          } ${className}`}
+          {...props}
+        >
+          {children}
+        </motion.button>
+      );
+  }
 };
 
-const Button: FC<Props> = ({ text, iconUrl, className, onClick }) => (
-  <button type="button" className={`flex flex-row gap-2 rounded-3xl bg-neutral-900 p-4 text-base text-white ${className}`} onClick={onClick}>
-    {iconUrl && <Image src={iconUrl} alt={`ボタン ${text} のアイコン画像`} />}
-    {text}
-  </button>
-);
+const ButtonIcon: FC<{ children: ReactNode }> = ({ children }) => {
+  const iconContextValue = useMemo(() => ({ size: '1.5rem' }), []);
+  return (
+    <div className="flex aspect-square max-h-6 items-center justify-center fill-current">
+      <IconContext.Provider value={iconContextValue}>{children}</IconContext.Provider>
+    </div>
+  );
+};
 
 Button.defaultProps = {
-  text: '',
-  iconUrl: '',
-  className: '',
+  outlined: false,
 };
 
-export default Button;
+export { Button, ButtonIcon };
