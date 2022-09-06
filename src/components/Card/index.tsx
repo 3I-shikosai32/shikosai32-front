4a6c4d41c8ba83b0369974c661fdbafa53b991a5
ReoHakase <reonaldhkt+webs@gmail.com>
2022-09-06 13:21:14 +0900

    feat: ✨ (Card) Cardコンポーネントの作成
    `framer-motion`でアニメーション可能なラップ済みコンポーネント`MotionCard`も利用可能に。

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,26 @@
+import { motion } from 'framer-motion';
+import type { FC, ComponentPropsWithRef, ReactNode } from 'react';
+import { forwardRef } from 'react';
+import twMerge from '@/libs/twmerge';
+
+export type CardProps = ComponentPropsWithRef<'div'> & {
+  className?: string;
+  children?: ReactNode;
+};
+
+// `framer-motion`でアニメーションを付加できるようにするため`forwardRef`でrefを受け取れるようにする
+// 型推論はできているのに`<HTMLDivElement, CardProps>`で明示的に型ジェネリクスを指定しないとeslint`react/prop-types`エラーになるため指定
+export const Card: FC<CardProps> = forwardRef<HTMLDivElement, CardProps>(({ className, children, ...props }, ref) => (
+  <div ref={ref} className={twMerge('rounded-3xl shadow-xl bg-white', className)} {...props}>
+    {children}
+  </div>
+));
+
+Card.displayName = 'Card';
+Card.defaultProps = {
+  className: '',
+  children: null,
+};
+
+// `framer-motion`でアニメーション可能なCardコンポーネント
+export const MotionCard = motion(Card);
