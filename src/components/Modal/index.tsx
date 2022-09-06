afdd52d6304bb3066ae92b613d63ec861d3225b9
ReoHakase <reonaldhkt+webs@gmail.com>
2022-09-07 05:40:26 +0900

    doc: 📝 (Modal) `<Modal>`コンポーネントの修正とストーリーの追加

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -8,17 +8,15 @@ import twMerge from '@/libs/twmerge';
 type ModalContentProps = MotionCardProps;
 
 const ModalContent: FC<ModalContentProps> = ({ className, children, ...props }) => (
-  <AnimatePresence>
-    <MotionCard
-      initial={{ opacity: 0, scale: 1.2 }}
-      animate={{ opacity: 1, scale: 1.0 }}
-      exit={{ opacity: 0, scale: 0.8 }}
-      className={twMerge('p-6 gap-4', className)}
-      {...props}
-    >
-      {children}
-    </MotionCard>
-  </AnimatePresence>
+  <MotionCard
+    initial={{ opacity: 0, scale: 1.2 }}
+    animate={{ opacity: 1, scale: 1.0 }}
+    exit={{ opacity: 0, scale: 0.8 }}
+    className={twMerge('relative p-6 gap-4 max-w-lg shadow-z32', className)}
+    {...props}
+  >
+    {children}
+  </MotionCard>
 );
 
 export type ModalTitleProps = ComponentPropsWithoutRef<typeof Dialog.Title> & {
@@ -50,13 +48,16 @@ export type ModalProps = Pick<ComponentPropsWithoutRef<typeof Dialog.Root>, 'ope
 
 // モーダルのトリガーとなるボタンとモーダルの中身を受け取り、モーダルを表示する本体となるコンポーネント
 export const Modal: FC<ModalProps> = ({ open, onOpenChange, trigger, children }) => (
-  <Dialog.Root open={open} onOpenChange={onOpenChange}>
-    <Dialog.Trigger asChild>{trigger}</Dialog.Trigger>
-    <Dialog.Portal>
-      <Dialog.Overlay />
-      <Dialog.Content asChild>
-        <ModalContent>{children}</ModalContent>
-      </Dialog.Content>
-    </Dialog.Portal>
-  </Dialog.Root>
+  <AnimatePresence>
+    <Dialog.Root open={open} onOpenChange={onOpenChange}>
+      <Dialog.Trigger asChild>{trigger}</Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 z-50 flex min-h-screen w-screen items-center justify-center bg-neutral-900/50">
+          <Dialog.Content asChild>
+            <ModalContent>{children}</ModalContent>
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </Dialog.Root>
+  </AnimatePresence>
 );
