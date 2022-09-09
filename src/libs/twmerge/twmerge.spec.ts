3cd23fc810c05dfe5913cbf8277c572c7c98977c
ReoHakase <reonaldhkt+webs@gmail.com>
2022-09-09 17:48:51 +0900

    refactor: ♻️ (src/libs/twmerge/twmerge.spec.ts) `tailwind-merge`のテストコードの記述を`it()`から`test()`に変更した。

--- src/libs/twmerge/twmerge.spec.ts
+++ src/libs/twmerge/twmerge.spec.ts
@@ -1,17 +1,17 @@
 import twMerge, { isGradientName } from '.';
 
 describe('twMerge', () => {
-  it('独自クラス"shadow-z*"の対立を解決できる', () => {
+  test('独自クラス"shadow-z*"の対立を解決できる', () => {
     expect(twMerge('shadow-z4 shadow-z8')).toBe('shadow-z8');
     expect(twMerge('shadow-xl shadow-z8')).toBe('shadow-z8');
   });
-  it('独自クラス"gradient-*"の対立を解決できる', () => {
+  test('独自クラス"gradient-*"の対立を解決できる', () => {
     expect(twMerge('gradient-primary gradient-game')).toBe('gradient-game');
   });
-  it('独自クラス"rounded-base"の対立を解決できる', () => {
+  test('独自クラス"rounded-base"の対立を解決できる', () => {
     expect(twMerge('rounded-base rounded-3xl')).toBe('rounded-3xl');
   });
-  it('isGradientName()がグラデーション名を識別できる', () => {
+  test('isGradientName()がグラデーション名を識別できる', () => {
     expect(isGradientName('primary')).toBe(true);
   });
 });
