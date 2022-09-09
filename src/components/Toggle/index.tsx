f8ce39debe2387aa0086f83ca3a77ec73aabfdab
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-09-10 00:33:27 +0900

    fix: 🐛 (Toggle)  // eslint-disable-next-line tailwindcss/classnames-orderの消去

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -9,14 +9,13 @@ const Toggle: FC<{ onClick?: () => void; options: ToggleProps }> = ({ onClick, o
   switch (size) {
     case 'large':
       return (
-        <div >
+        <div>
           <label htmlFor="toggle">
             <input id="toggle" type="checkbox" onClick={onClick} className="peer sr-only" />
             <span
-              // eslint-disable-next-line tailwindcss/classnames-order
               className={`block w-[6em] cursor-pointer rounded-full 
-             bg-gray-400 shadow-inner
-             p-[3px] after:block after:h-[2.8em] after:w-[2.8em] after:rounded-full 
+             bg-gray-400 p-[3px]
+             shadow-inner after:block after:h-[2.8em] after:w-[2.8em] after:rounded-full 
              after:bg-white after:transition
              peer-checked:after:translate-x-[calc(100%-0px)]`}
             />
@@ -29,9 +28,8 @@ const Toggle: FC<{ onClick?: () => void; options: ToggleProps }> = ({ onClick, o
           <label htmlFor="toggle">
             <input id="toggle" type="checkbox" onClick={onClick} className="peer sr-only" />
             <span
-              // eslint-disable-next-line tailwindcss/classnames-order
-              className={`shadow-lg block w-[4.5em] cursor-pointer rounded-full bg-gray-400
-              p-[4px] after:block after:h-[1.9em] after:w-[1.9em] after:rounded-full 
+              className={`block w-[4.5em] cursor-pointer rounded-full bg-gray-400 p-[4px]
+              shadow-lg after:block after:h-[1.9em] after:w-[1.9em] after:rounded-full 
               after:bg-white after:transition 
               peer-checked:after:translate-x-[calc(100%-(-5px))]`}
             />
@@ -44,10 +42,8 @@ const Toggle: FC<{ onClick?: () => void; options: ToggleProps }> = ({ onClick, o
           <label htmlFor="toggle">
             <input id="toggle" type="checkbox" onClick={onClick} className="peer sr-only" />
             <span
-              // eslint-disable-next-line tailwindcss/classnames-order
-              className={`shadow-lg block w-[2.7em] cursor-pointer rounded-full 
-           bg-gray-400
-             p-[3px] after:block after:h-[1.1em] after:w-[1.1em] after:rounded-full 
+              className={`block w-[2.7em] cursor-pointer rounded-full bg-gray-400 p-[3px]
+             shadow-lg after:block after:h-[1.1em] after:w-[1.1em] after:rounded-full 
              after:bg-white after:transition 
              peer-checked:after:translate-x-[calc(100%-(-1.3px))]`}
             />
