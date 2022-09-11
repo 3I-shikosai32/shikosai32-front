88631c5ee4f3ce25c3fc32ab5ed96a42b2b54d02
ReoHakase <reonaldhkt+webs@gmail.com>
2022-09-12 07:16:59 +0900

    doc: 📝 (AudioControlMenu) 曲の情報の表示と再生状態の操作を行うコンポーネント`AudioControlMenu`のストーリーを追加した。

---
+++ src/layouts/components/AudioControlMenu/AudioControlMenu.story.tsx
@@ -0,0 +1,67 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { action } from '@storybook/addon-actions';
+import type { ComponentStoryObj, ComponentMeta } from '@storybook/react';
+
+import { AudioControlMenu } from './index';
+
+type Story = ComponentStoryObj<typeof AudioControlMenu>;
+
+const meta: ComponentMeta<typeof AudioControlMenu> = {
+  component: AudioControlMenu,
+  args: {
+    name: '栄の活躍:Remix',
+    composers: [
+      {
+        name: '酒井晴渚',
+        social: 'https://example.com',
+      },
+    ],
+    isPlaying: true,
+    setIsPlaying: action('setIsPlaying'),
+  },
+  argTypes: {
+    className: {
+      description: 'メニューを開くボタンのサイズ等のスタイルを変更するために用意されている。`aspect-square`で縦横比が1:1に固定されています。',
+      control: { type: 'text' },
+    },
+    viewportClassName: {
+      description:
+        '`<NavigationMenu>`参照。吹き出しメニュー`<NavigationContent>`を挿入する先の親要素のスタイルを変更するために使用できる。例えば、`left: 0; justify-content: start;` `right: 0; justify-content: end;`を設定することで、吹き出しメニューのよる方向を変更できる。また、親である`<NavigationMenu>`の幅よりも大きい`width`を設定したい場合にも`w-48`等で使用できる。',
+      control: { type: 'text' },
+    },
+    name: {
+      description: '現在読み込まれている曲の名前',
+      control: { type: 'text' },
+    },
+    composers: {
+      description: '現在読み込まれている曲の作曲者',
+      control: { type: 'none' },
+    },
+    isPlaying: {
+      description: '現在再生中かどうか',
+      control: { type: 'boolean' },
+    },
+    setIsPlaying: {
+      description: '再生中かどうかを変更する関数。`isPlaying`とセットで使用する。',
+      control: { type: 'none' },
+    },
+  },
+};
+
+export default meta;
+
+export const Default: Story = {};
+
+export const WithMultipleComposers: Story = {
+  args: {
+    composers: [
+      {
+        name: '酒井晴渚',
+        social: 'https://example.com',
+      },
+      {
+        name: '酒井晴渚',
+      },
+    ],
+  },
+};
