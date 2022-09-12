29448f3a2cc4bef5956212c153639765dfdea980
ReoHakase <reonaldhkt+webs@gmail.com>
2022-09-12 15:59:21 +0900

    feat: ✨ (ShareButton) `<ShareButton>`を追加した。

---
+++ src/layouts/components/ShareButton/index.tsx
@@ -0,0 +1,58 @@
+import type { FC } from 'react';
+import { useState, useCallback } from 'react';
+import { RiShareFill } from 'react-icons/ri';
+import { Button, ButtonProps, ButtonIcon } from '@/components/Button';
+import { Modal, ModalOverlay, ModalContent, ModalTitle, ModalDescription } from '@/components/Modal';
+
+export type ShareButtonProps = Pick<ButtonProps, 'className'> & {
+  alwaysAlternative?: boolean;
+};
+
+export const ShareButton: FC<ShareButtonProps> = ({ alwaysAlternative, className, ...props }) => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const onCloseButtonClick = useCallback(() => {
+    setIsModalOpen(false);
+  }, [setIsModalOpen]);
+  const onShareButtonClick = useCallback(async () => {
+    try {
+      if (alwaysAlternative) throw new Error();
+      await navigator.share({
+        title: document.title,
+        text: 'OZ at 3Iで現実のボードゲームとスマホを組み合わせたオンラインゲームを楽しもう！',
+        url: window.location.href,
+      });
+    } catch (error) {
+      setIsModalOpen(true);
+    }
+  }, [setIsModalOpen, alwaysAlternative]);
+  const onModalOpenChange = useCallback((isOpen: boolean) => setIsModalOpen(isOpen), [setIsModalOpen]);
+
+  return (
+    <>
+      <Button onClick={onShareButtonClick} circle ghost {...props}>
+        <ButtonIcon>
+          <RiShareFill />
+        </ButtonIcon>
+      </Button>
+      <Modal open={isModalOpen} onOpenChange={onModalOpenChange} trigger={<button type="button" aria-hidden className="hidden" />}>
+        <ModalOverlay>
+          <ModalContent>
+            <ModalTitle>OZ at 3Iを友達に広めよう！</ModalTitle>
+            <ModalDescription>
+              このリンクをコピーしてあなたの友達と共有しよう！
+              <span className="block overflow-hidden rounded-base bg-neutral-100 p-3 font-branding text-xs text-neutral-700">
+                {window.location.href}
+              </span>
+            </ModalDescription>
+            <Button onClick={onCloseButtonClick}>閉じる</Button>
+          </ModalContent>
+        </ModalOverlay>
+      </Modal>
+    </>
+  );
+};
+
+ShareButton.defaultProps = {
+  alwaysAlternative: false,
+};
