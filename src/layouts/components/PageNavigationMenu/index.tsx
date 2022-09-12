b7353963052f4c52bd3ee5ef831c917da96c0096
ReoHakase <reonaldhkt+webs@gmail.com>
2022-09-12 16:06:37 +0900

    refactor: ♻️ (PageNavigationMenu) `<PageNavigationMenu>`内の不要な`<div>`を取り除いた。

--- src/layouts/components/PageNavigationMenu/index.tsx
+++ src/layouts/components/PageNavigationMenu/index.tsx
@@ -9,40 +9,32 @@ export type PageNavigationMenuProps = Pick<NavigationMenuProps, 'className'>;
 export const PageNavigationMenu: FC<PageNavigationMenuProps> = ({ className, ...props }) => (
   <NavigationMenu className={twMerge('flex p-2 justify-center gap-6 items-center', className)} {...props}>
     <NavigationItem>
-      <div>
-        <NavigationLink>
-          <Link className="text-neutral-700 drop-shadow-none" href="https://example.com">
-            ランキング
-          </Link>
-        </NavigationLink>
-      </div>
+      <NavigationLink>
+        <Link className="text-neutral-700 drop-shadow-none" href="https://example.com">
+          ランキング
+        </Link>
+      </NavigationLink>
     </NavigationItem>
     <NavigationItem>
-      <div>
-        <NavigationLink>
-          <Link className="text-neutral-700 drop-shadow-none" href="https://example.com">
-            ゲーム一覧
-          </Link>
-        </NavigationLink>
-      </div>
+      <NavigationLink>
+        <Link className="text-neutral-700 drop-shadow-none" href="https://example.com">
+          ゲーム一覧
+        </Link>
+      </NavigationLink>
     </NavigationItem>
     <NavigationItem>
-      <div>
-        <NavigationLink>
-          <Link className="text-neutral-700 drop-shadow-none" href="https://example.com">
-            景品交換
-          </Link>
-        </NavigationLink>
-      </div>
+      <NavigationLink>
+        <Link className="text-neutral-700 drop-shadow-none" href="https://example.com">
+          景品交換
+        </Link>
+      </NavigationLink>
     </NavigationItem>
     <NavigationItem>
-      <div>
-        <NavigationLink>
-          <Link className="text-neutral-700 drop-shadow-none" href="https://example.com">
-            スタッフ
-          </Link>
-        </NavigationLink>
-      </div>
+      <NavigationLink>
+        <Link className="text-neutral-700 drop-shadow-none" href="https://example.com">
+          スタッフ
+        </Link>
+      </NavigationLink>
     </NavigationItem>
   </NavigationMenu>
 );
