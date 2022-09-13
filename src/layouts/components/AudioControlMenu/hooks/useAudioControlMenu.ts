fba2ddc61df24b6b34680e425f21542f98f94229
ReoHakase <reonaldhkt+webs@gmail.com>
2022-09-13 11:35:04 +0900

    feat: ✨ (AudioControlMenu) `<AudioControlMenu>`のステートを管理するカスタムフック`useAudioControlMenu`を追加した。

---
+++ src/layouts/components/AudioControlMenu/hooks/useAudioControlMenu.ts
@@ -0,0 +1,18 @@
+import type { AudioControlMenuStateProps } from '../index';
+import { useAudioControlValue } from '@/state/audio/audioControl';
+import { useAudioResourceValue } from '@/state/audio/audioResource';
+
+const useAudioControlMenu = (): AudioControlMenuStateProps => {
+  const audioResource = useAudioResourceValue();
+  const name = audioResource?.name || '曲が選ばれていません';
+  const composers = audioResource?.composers || [];
+  const { isPlaying, setIsPlaying } = useAudioControlValue();
+  return {
+    name,
+    composers,
+    isPlaying,
+    setIsPlaying,
+  };
+};
+
+export default useAudioControlMenu;
