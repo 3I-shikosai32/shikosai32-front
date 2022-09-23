0775220a3015201a2d60ee63aa5efd1f5e373fad
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-09-24 01:33:30 +0900

    fix: 🐛 (Loading) Loadingにおけるバグの修正

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -3,7 +3,10 @@ import twMerge from '@/libs/twmerge';
 
 const Loading: FC<{ size: number; fat: boolean }> = ({ size, fat }) => (
   <div className="flex justify-center">
-    <div className={twMerge('animate-spin rounded-full  border-pink-500 border-t-transparent',fat ? 'border-8':'border-4')} style={{ width: size, height: size }} />
+    <div
+      className={twMerge('animate-spin rounded-full  border-pink-500 border-t-transparent', fat ? 'border-8' : 'border-4')}
+      style={{ width: size, height: size }}
+    />
   </div>
 );
 
