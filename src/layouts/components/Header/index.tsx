d814e65c99aaa3a3d577e656b5449eef9dd9649b
ReoHakase <reonaldhkt+webs@gmail.com>
2022-09-24 05:10:38 +0900

    feat: ✨ (Header) `<Header>`に`<ShareButton>`を追加した。また、`<UserNavigationMenu>` `<AudioControlMenu>`をそれぞれ副作用を持つものに置き換えた。

--- src/layouts/components/Header/index.tsx
+++ src/layouts/components/Header/index.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image';
 import type { FC, ComponentPropsWithoutRef } from 'react';
-import { AudioControlMenu } from '../AudioControlMenu';
+import { AudioControlMenuContainer } from '../AudioControlMenu';
 import { HamburgerMenu } from '../HamburgerMenu';
 import { PageNavigationMenu } from '../PageNavigationMenu';
-import { UserNavigationMenu } from '../UserNavigationMenu';
+import { ShareButton } from '../ShareButton';
+import { UserNavigationMenuContainer } from '../UserNavigationMenu';
 import twMerge from '@/libs/twmerge';
 
 const audioControlProps = {
@@ -24,13 +25,18 @@ export const Header: FC<HeaderProps> = ({ className, ...props }) => (
   <header className={twMerge('bg-white w-full flex flex-col justify-center items-center p-0', className)} {...props}>
     <div className="flex w-full flex-row items-center justify-center p-2">
       <div className="flex flex-1 flex-row items-center justify-start p-0">
-        <UserNavigationMenu className="min-h-12 h-12" />
+        <UserNavigationMenuContainer className="min-h-12 h-12" />
+        <ShareButton className="min-h-12 ml-1 h-12" />
       </div>
       <figure className="relative flex h-full w-14 flex-none items-center justify-center">
         <Image src="/logos/header.png" width={324} height={284} alt="OZ at 3Iのロゴ画像" />
       </figure>
       <div className="flex flex-1 flex-row items-center justify-end p-0">
-        <AudioControlMenu className="min-h-12 h-12" viewportClassName="left-auto right-[-3.5rem] md:right-0 justify-end" {...audioControlProps} />
+        <AudioControlMenuContainer
+          className="min-h-12 h-12"
+          viewportClassName="left-auto right-[-3.5rem] md:right-0 justify-end"
+          {...audioControlProps}
+        />
         <HamburgerMenu className="min-h-12 h-12 md:hidden" />
       </div>
     </div>
