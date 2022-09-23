5a86486f85898f21d79f051aa886032095fb9b9f
ReoHakase <reonaldhkt+webs@gmail.com>
2022-09-24 05:09:19 +0900

    doc: 📝 (Header) `<Header>`のストーリーに`RecoilRoot`を追加した。

--- src/layouts/components/Header/Header.story.tsx
+++ src/layouts/components/Header/Header.story.tsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import type { ComponentStoryObj, ComponentMeta } from '@storybook/react';
+import { RecoilRoot } from 'recoil';
 
 import { Header } from './index';
 
@@ -7,6 +8,7 @@ type Story = ComponentStoryObj<typeof Header>;
 
 const meta: ComponentMeta<typeof Header> = {
   component: Header,
+  decorators: [(story) => <RecoilRoot>{story()}</RecoilRoot>],
   argTypes: {
     className: {
       description: '`position: sticky`やサイズの変更のために指定できます。',
