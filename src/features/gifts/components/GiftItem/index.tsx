e06215216bfaaab4b5c58e3742221b96e3aa1445
ReoHakase <reonaldhkt+webs@gmail.com>
2022-09-24 21:11:23 +0900

    refactor: ♻️ (GiftItem) 灰色のグラデーション`gradient-exchange-itemframe`を使用するよう変更した。

--- src/features/gifts/components/GiftItem/index.tsx
+++ src/features/gifts/components/GiftItem/index.tsx
@@ -27,7 +27,7 @@ export const GiftItem: FC<GiftItemProps> = ({ consumablePoint, name, iconUrl, pr
 
   return (
     <div className={twMerge('flex flex-row max-w-sm gap-5 p-4', className)} {...props}>
-      <figure className="flex aspect-square h-32 items-center justify-center rounded-full bg-[#CCD6E5] shadow-z16">
+      <figure className="flex aspect-square h-32 items-center justify-center rounded-full bg-gradient-to-b shadow-z16 gradient-exchange-itemframe">
         <div className="relative aspect-square h-3/4">
           <Image src={iconUrl} objectFit="contain" width={256} height={256} alt={`景品:${name}の画像`} />
         </div>
@@ -58,7 +58,7 @@ export const GiftItem: FC<GiftItemProps> = ({ consumablePoint, name, iconUrl, pr
           trigger={
             <Button
               disabled={!isAvailable}
-              className={twMerge('w-fit disabled:contrast-100', !isAvailable ? 'bg-[#CCD6E5]' : 'bg-gradient-to-br gradient-exchange')}
+              className={twMerge('w-fit disabled:contrast-100 bg-gradient-to-br gradient-exchange', !isAvailable && 'gradient-exchange-itemframe')}
             >
               {!isInStock && (
                 <>
