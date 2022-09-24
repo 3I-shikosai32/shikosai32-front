f97b1134a2144e826464c00022d4cf721516acaa
ReoHakase <reonaldhkt+webs@gmail.com>
2022-09-24 20:23:20 +0900

    wip: 🚧 (UserNavigationMenu) カスタムフック`useNavigationMenu`を名前付きexportするように変更した。
    ChromaticのCIのエラー回避のため、、これで修正できるかは不明

--- src/layouts/components/UserNavigationMenu/hooks/useUserNavigationMenu.ts
+++ src/layouts/components/UserNavigationMenu/hooks/useUserNavigationMenu.ts
@@ -3,7 +3,8 @@ import { Role } from '@/libs/graphql/generated/graphql';
 import { useFindUserBio } from '@/libs/graphql/handlers/query/FindUserBio';
 import authActions from '@/state/authState';
 
-const useUserNavigation = (): UserNavigationMenuStateProps => {
+// eslint-disable-next-line import/prefer-default-export
+export const useUserNavigation = (): UserNavigationMenuStateProps => {
   const firebaseUser = authActions.useCurrentUser();
   const { data, fetching, error } = useFindUserBio({ uid: firebaseUser?.uid });
   if (!data || fetching || error) {
@@ -15,5 +16,3 @@ const useUserNavigation = (): UserNavigationMenuStateProps => {
     isLoggedIn: !!data.findUser?.id,
   };
 };
-
-export default useUserNavigation;
