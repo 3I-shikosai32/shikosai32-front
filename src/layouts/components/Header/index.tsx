2534cc412c11fadfb9ae5c1b62f422e889d9f64e
ReoHakase <reonaldhkt+webs@gmail.com>
2022-09-25 14:27:36 +0900

    wip: 🚧 (Header) `<Header>`から副作用を含む`<~Container>`を排除した。

--- src/layouts/components/Header/index.tsx
+++ src/layouts/components/Header/index.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
 import type { FC, ComponentPropsWithoutRef } from 'react';
-import { AudioControlMenuContainer } from '../AudioControlMenu/container';
+import { AudioControlMenu } from '../AudioControlMenu';
 import { HamburgerMenu } from '../HamburgerMenu';
 import { PageNavigationMenu } from '../PageNavigationMenu';
 import { ShareButton } from '../ShareButton';
-import { UserNavigationMenuContainer } from '../UserNavigationMenu/container';
+import { UserNavigationMenu } from '../UserNavigationMenu';
 import twMerge from '@/libs/twmerge';
 
 const audioControlProps = {
@@ -25,18 +25,14 @@ export const Header: FC<HeaderProps> = ({ className, ...props }) => (
   <header className={twMerge('bg-white w-full flex flex-col justify-center items-center p-0', className)} {...props}>
     <div className="flex w-full flex-row items-center justify-center p-2">
       <div className="flex flex-1 flex-row items-center justify-start p-0">
-        <UserNavigationMenuContainer className="min-h-12 h-12" />
+        <UserNavigationMenu className="min-h-12 h-12" />
         <ShareButton className="min-h-12 ml-1 h-12" />
       </div>
       <figure className="relative flex h-full w-14 flex-none items-center justify-center">
         <Image src="/logos/header.png" width={324} height={284} alt="OZ at 3Iのロゴ画像" />
       </figure>
       <div className="flex flex-1 flex-row items-center justify-end p-0">
-        <AudioControlMenuContainer
-          className="min-h-12 h-12"
-          viewportClassName="left-auto right-[-3.5rem] md:right-0 justify-end"
-          {...audioControlProps}
-        />
+        <AudioControlMenu className="min-h-12 h-12" viewportClassName="left-auto right-[-3.5rem] md:right-0 justify-end" {...audioControlProps} />
         <HamburgerMenu className="min-h-12 h-12 md:hidden" />
       </div>
     </div>
