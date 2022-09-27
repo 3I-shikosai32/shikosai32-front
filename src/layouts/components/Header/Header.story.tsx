1b46073fc22fa976401700c47c848926a2cd20c7
ReoHakase <reonaldhkt+webs@gmail.com>
2022-09-28 00:25:21 +0900

    doc: 📝 (Header) `<Header>`のストーリーで、Firebaseに依存する`useUserNavigationMenu`を、依存しないモックフックに置き換えるようにした。
    `react-magnetic-di`を使用した。

--- src/layouts/components/Header/Header.story.tsx
+++ src/layouts/components/Header/Header.story.tsx
@@ -1,14 +1,23 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import type { ComponentStoryObj, ComponentMeta } from '@storybook/react';
+import { DiProvider, injectable } from 'react-magnetic-di';
 import { RecoilRoot } from 'recoil';
-
 import { Header } from './index';
+import { useUserNavigationMenu } from '@/layouts/components/UserNavigationMenu/hooks/useUserNavigationMenu';
 
 type Story = ComponentStoryObj<typeof Header>;
 
+const injectedHooks = [
+  injectable(useUserNavigationMenu, () => ({
+    showAdminLink: true,
+    userIconUrl: '/icons/fox.png',
+    isLoggedIn: true,
+  })),
+];
+
 const meta: ComponentMeta<typeof Header> = {
   component: Header,
-  decorators: [(story) => <RecoilRoot>{story()}</RecoilRoot>],
+  decorators: [(story) => <RecoilRoot>{story()}</RecoilRoot>, (story) => <DiProvider use={injectedHooks}>{story()}</DiProvider>],
   argTypes: {
     className: {
       description: '`position: sticky`やサイズの変更のために指定できます。',
