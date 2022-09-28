3fb3decbe035ec717df3622e6d5bb61efcd94061
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-09-28 20:30:14 +0900

    feat: ✨ (new-user) フォームコンポーネントの作成

---
+++ src/features/new-user/components/form.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import type { FC, ChangeEvent } from 'react';
+
+type FormProps = {
+  name: string;
+  updateName: (event: ChangeEvent<HTMLInputElement>) => void;
+  email: string;
+  updateEmail: (event: ChangeEvent<HTMLInputElement>) => void;
+};
+const Form: FC<FormProps> = ({ name, updateName, email, updateEmail }) => (
+  <div className="mx-16">
+    <form>
+      <div className="group relative z-0 mb-6 w-full">
+        <input
+          value={name}
+          onChange={updateName}
+          type="text"
+          id="floating_password"
+          className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 "
+          placeholder=" "
+          required
+          size={100}
+        />
+        <label
+          className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 dark:text-gray-400 peer-focus:dark:text-blue-500"
+        >
+          your nickname
+        </label>
+      </div>
+      <div className="group relative z-0 mb-6 w-full">
+        <input
+          value={email}
+          onChange={updateEmail}
+          type="text"
+          id="floating_password"
+          className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 "
+          placeholder=" "
+          required
+          size={100}
+        />
+        <label
+          className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 dark:text-gray-400 peer-focus:dark:text-blue-500"
+        >
+          your email
+        </label>
+      </div>
+      
+    </form>
+  </div>
+);
+export default Form;
