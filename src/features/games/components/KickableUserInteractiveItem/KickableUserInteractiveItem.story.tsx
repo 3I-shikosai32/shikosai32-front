d99213690b073f92065a79e177f41ddc4b602e48
ReoHakase <reonaldhkt+webs@gmail.com>
2022-09-30 20:59:58 +0900

    doc: 📝 (KickableUserInteractiveItem) ゲーム管理ページ用の参加ユーザーをキックするためUIを提供するコンポーネント<KickableUserInteractiveItem>のストーリーを作成した。

---
+++ src/features/games/components/KickableUserInteractiveItem/KickableUserInteractiveItem.story.tsx
@@ -0,0 +1,38 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import type { ComponentStoryObj, ComponentMeta } from '@storybook/react';
+
+import { KickableUserInteractiveItem } from './index';
+
+type Story = ComponentStoryObj<typeof KickableUserInteractiveItem>;
+
+const meta: ComponentMeta<typeof KickableUserInteractiveItem> = {
+  component: KickableUserInteractiveItem,
+  args: {
+    id: 'this-is-user-id-12345',
+    name: 'ユーザー名',
+    iconUrl: '/icons/fox.png',
+    className: 'bg-white max-w-md',
+  },
+  argTypes: {
+    id: {
+      description: 'ユーザーのIDを指定する。確認のためユーザー名とともに表示される。また、内部ではコンポーネントの`key`として使用される。',
+      control: { type: 'text' },
+    },
+    name: {
+      description: 'ユーザーの名前を指定する。',
+      control: { type: 'text' },
+    },
+    iconUrl: {
+      description: 'ユーザーのアイコンのURLを指定する。',
+      control: { type: 'text' },
+    },
+    className: {
+      description: 'サイズを変更するために用意されている。',
+      control: { type: 'text' },
+    },
+  },
+};
+
+export default meta;
+
+export const Default: Story = {};
