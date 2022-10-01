c4c48633815bab44b026fc5beb6c6cf87653efac
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-01 18:33:21 +0900

    fix: 🐛 (new-user) charactersの間違った画像を修正

--- src/features/new-user/components/character.tsx
+++ src/features/new-user/components/character.tsx
@@ -10,14 +10,14 @@ type CharacterProps = {
 const Character: FC<CharacterProps> = ({ checkedItems, handleChange }) => {
   const characters = ['cat', 'fox', 'goku', 'pudding', 'reaper', 'tree'];
   return (
-    <div className="grid grid-cols-3 px-5">
+    <div className=" grid grid-cols-3 px-5">
       {characters.map((character) => (
-        <div key={character} className="relative my-5 h-32 rounded-2xl">
-          <div className="top-5 m-1">
-            <input id={character} type="checkbox" className="absolute h-5 w-5" checked={checkedItems === character} onChange={handleChange} />
+        <div key={character} className="my-5 mx-1 h-[150px]  rounded-2xl bg-white shadow-2xl">
+          <div className="top-5 m-[0.5px]">
+            <input id={character} type="checkbox" className="h-5 w-5" checked={checkedItems === character} onChange={handleChange} />
           </div>
           <div className="flex justify-center">
-            <Image src={`/selecticons/${character}.png`} width={140} height={140} alt="My icon" />
+            <Image src={`/characters/${character}.svg`} width={120} height={120} alt="My icon" />
           </div>
         </div>
       ))}
