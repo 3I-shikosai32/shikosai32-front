d3fbeeb1d69b9cc5627e98fb8826311247dfaa18
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-01 18:35:14 +0900

    fix: 🐛 (new-user) indexにおいて、idがundefiedまたはnullにならないように修正

--- src/features/new-user/index.tsx
+++ src/features/new-user/index.tsx
@@ -1,23 +1,25 @@
 import type { FC } from 'react';
-import Submit from './components/Submit';
-import Top from './components/Top';
 import Character from './components/character';
 import Form from './components/form';
+import Submit from './components/submit';
+import Top from './components/top';
 import useCharacter from './hooks/useCharacter';
 import useForm from './hooks/useForm';
 import authActions from '@/state/authState';
 
 const NewUser: FC = () => {
   const user = authActions.useCurrentUser();
-  const { value: email, updateValue: updateEmail } = useForm({ initialvalue: 'hoge' });
-  const { value: name, updateValue: updateName } = useForm({ initialvalue: 'hoge' });
+  const id = user?.uid ? user.uid : '';
+  const { value: email, updateValue: updateEmail } = useForm({ initialvalue: user?.email });
+  const { value: name, updateValue: updateName } = useForm({ initialvalue: user?.displayName });
   const { checkedItems, checkedCharcter, handleChange } = useCharacter();
+
   return (
     <div>
       <Top />
       <Form email={email} updateEmail={updateEmail} name={name} updateName={updateName} />
-      <Character checkedItems={checkedItems} handleChange={handleChange} checkboxSize={5} boxMergin="m-3" imageSize={70}/>
-      <Submit />
+      <Character checkedItems={checkedItems} handleChange={handleChange} />
+      <Submit email={email} name={name} character={checkedCharcter} id={id} />
     </div>
   );
 };
