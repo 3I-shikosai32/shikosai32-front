f29e5d19be872c1e1b98fa032a6cb3138e365587
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-01 14:59:48 +0900

    fix: 🐛 (new-user) 使用していなかったidを消去

--- src/features/new-user/components/form.tsx
+++ src/features/new-user/components/form.tsx
@@ -15,15 +15,12 @@ const Form: FC<FormProps> = ({ name, updateName, email, updateEmail }) => (
           value={name}
           onChange={updateName}
           type="text"
-          id="floating_password"
           className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 "
           placeholder=" "
           required
           size={100}
         />
-        <label
-          className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 dark:text-gray-400 peer-focus:dark:text-blue-500"
-        >
+        <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 dark:text-gray-400 peer-focus:dark:text-blue-500">
           your nickname
         </label>
       </div>
@@ -32,19 +29,15 @@ const Form: FC<FormProps> = ({ name, updateName, email, updateEmail }) => (
           value={email}
           onChange={updateEmail}
           type="text"
-          id="floating_password"
           className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 "
           placeholder=" "
           required
           size={100}
         />
-        <label
-          className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 dark:text-gray-400 peer-focus:dark:text-blue-500"
-        >
+        <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 dark:text-gray-400 peer-focus:dark:text-blue-500">
           your email
         </label>
       </div>
-      
     </form>
   </div>
 );
