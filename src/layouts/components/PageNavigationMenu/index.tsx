2180bd084f27895b2f909d2be670d89911ca8617
ReoHakase <reonaldhkt+webs@gmail.com>
2022-10-03 05:26:42 +0900

    fix: 🐛 (Header, PageNavigationMenu) Figmaの変更案の通りUIを修正した。

--- src/layouts/components/PageNavigationMenu/index.tsx
+++ src/layouts/components/PageNavigationMenu/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import type { FC } from 'react';
 import { Link } from '@/components/Link';
 import { NavigationMenu, NavigationItem, NavigationLink } from '@/components/Navigation';
@@ -7,31 +8,38 @@ import twMerge from '@/libs/twmerge';
 export type PageNavigationMenuProps = Pick<NavigationMenuProps, 'className'>;
 
 export const PageNavigationMenu: FC<PageNavigationMenuProps> = ({ className, ...props }) => (
-  <NavigationMenu className={twMerge('flex p-2 justify-center gap-6 items-center', className)} {...props}>
+  <NavigationMenu className={twMerge('p-0 grid grid-cols-5 grid-rows-1 justify-center gap-4 items-center', className)} {...props}>
     <NavigationItem>
       <NavigationLink>
-        <Link className="text-neutral-700 drop-shadow-none" href="https://example.com">
+        <Link className="hidden text-center text-neutral-700 drop-shadow-none md:block" href="https://example.com">
           ランキング
         </Link>
       </NavigationLink>
     </NavigationItem>
     <NavigationItem>
       <NavigationLink>
-        <Link className="text-neutral-700 drop-shadow-none" href="https://example.com">
+        <Link className="hidden text-center text-neutral-700 drop-shadow-none md:block" href="https://example.com">
           ゲーム一覧
         </Link>
       </NavigationLink>
     </NavigationItem>
+    <NavigationItem className="md:px-4">
+      <NavigationLink>
+        <figure className="relative flex h-full w-14 flex-none items-center justify-center">
+          <Image src="/logos/header.png" width={324} height={284} alt="OZ at 3Iのロゴ画像" />
+        </figure>
+      </NavigationLink>
+    </NavigationItem>
     <NavigationItem>
       <NavigationLink>
-        <Link className="text-neutral-700 drop-shadow-none" href="https://example.com">
+        <Link className="hidden text-center text-neutral-700 drop-shadow-none md:block" href="https://example.com">
           景品交換
         </Link>
       </NavigationLink>
     </NavigationItem>
     <NavigationItem>
       <NavigationLink>
-        <Link className="text-neutral-700 drop-shadow-none" href="https://example.com">
+        <Link className="hidden text-center text-neutral-700 drop-shadow-none md:block" href="https://example.com">
           スタッフ
         </Link>
       </NavigationLink>
