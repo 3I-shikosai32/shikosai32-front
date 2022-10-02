3b227f968168a4b18d415197a6a6ebcf444e6e20
ReoHakase <reonaldhkt+webs@gmail.com>
2022-10-03 07:27:52 +0900

    fix: 🐛 (Header) 非表示のページナビゲーションメニューが上に重なることでボタンにインタラクトできない問題を修正した。

--- src/layouts/components/Header/index.tsx
+++ src/layouts/components/Header/index.tsx
@@ -10,7 +10,10 @@ export type HeaderProps = Omit<ComponentPropsWithoutRef<'header'>, 'children'>;
 
 export const Header: FC<HeaderProps> = ({ className, ...props }) => (
   <header className={twMerge('bg-white w-full grid grid-cols-1 grid-rows-1 p-2', className)} {...props}>
-    <div className="col-span-full row-span-full m-0 flex flex-row items-center justify-between p-0 md:flex-row-reverse md:justify-start">
+    <div className="col-span-full row-span-full m-0 flex flex-row items-center justify-center p-0">
+      <PageNavigationMenu />
+    </div>
+    <div className="relative z-10 col-span-full row-span-full m-0 flex flex-row items-center justify-between p-0 md:flex-row-reverse md:justify-start">
       <div className="flex flex-row items-center justify-start gap-1 p-0 md:flex-row-reverse">
         <UserNavigationMenuContainer className="min-h-12 h-12" viewportClassName="md:left-auto md:right-0 md:justify-end" />
         <ShareButton className="min-h-12 h-12 grow-0" />
@@ -19,9 +22,6 @@ export const Header: FC<HeaderProps> = ({ className, ...props }) => (
         <AudioControlMenuContainer className="min-h-12 h-12" viewportClassName="left-auto right-[-3.5rem] md:right-0 justify-end" />
         <HamburgerMenu className="min-h-12 h-12 md:hidden" />
       </div>
-    </div>
-    <div className="col-span-full row-span-full m-0 flex flex-row items-center justify-center p-0">
-      <PageNavigationMenu />
     </div>
   </header>
 );
