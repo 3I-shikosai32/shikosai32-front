91eadf4fcb256edddfa3d0eaedda6ad001dc1077
ReoHakase <reonaldhkt+webs@gmail.com>
2022-10-03 03:49:06 +0900

    feat: ✨ (GiftTransactionItem) 景品交換の取引の情報を表すコンポーネント`<GiftTransactionItem>`を作成した。
    リファクタリング前の`src/features/history/components`下の`TransactionItem`と同等。

---
+++ src/features/history/components/GiftTransactionItem/index.tsx
@@ -0,0 +1,17 @@
+import type { FC } from 'react';
+import { IoMdSwap } from 'react-icons/io';
+import { TransactionItem, TransactionItemProps } from '@/components/TransactionItem';
+import type { GiftHistoryDataFragment } from '@/libs/graphql/generated/graphql';
+
+export type GiftTransactionItemData = Pick<GiftHistoryDataFragment, 'id' | 'isDelivered' | 'createdAt' | 'deliveredAt'> & {
+  user: Pick<GiftHistoryDataFragment['user'], 'name' | 'iconUrl'>;
+  exchangedGift: Pick<GiftHistoryDataFragment['exchangedGift'], 'name'>;
+};
+
+export type GiftTransactionItemProps = Omit<TransactionItemProps, 'children'> & GiftTransactionItemData;
+export const GiftTransactionItem: FC<GiftTransactionItemProps> = ({ exchangedGift, ...props }) => (
+  <TransactionItem {...props}>
+    <IoMdSwap className="text-lg text-neutral-500" />
+    {exchangedGift.name}
+  </TransactionItem>
+);
