a8049fa05f5070ad3ff942ba313dc4152873c512
ReoHakase <reonaldhkt+webs@gmail.com>
2022-10-05 01:07:22 +0900

    feat: ✨ (PointInputUserInteractiveItem) ユーザーの情報とともに、ポイントを入力できる増減ボタン付きの入力欄を表示できるコンポーネント`<PointInputUserInteractiveItem>`を作成した。

---
+++ src/features/games/components/PointInputUserInteractiveItem/index.tsx
@@ -0,0 +1,22 @@
+import type { FC, ReactElement } from 'react';
+import type { PointInputProps } from '../PointInput';
+import { UserInteractiveItem, UserInteractiveItemActionGroup } from '@/components/UserInteractiveItem';
+import { UserItem, UserItemIcon, UserItemBio, UserItemName } from '@/components/UserItem';
+import type { UserItemProps, UserItemData } from '@/components/UserItem';
+import twMerge from '@/libs/twmerge';
+
+export type PointInputUserInteractiveItemProps = Omit<UserItemProps, 'children'> &
+  UserItemData & {
+    children: ReactElement<PointInputProps>;
+  };
+export const PointInputUserInteractiveItem: FC<PointInputUserInteractiveItemProps> = ({ children, id, name, iconUrl, className, ...props }) => (
+  <UserInteractiveItem key={id} className={twMerge('p-2', className)} {...props}>
+    <UserItem>
+      <UserItemIcon iconUrl={iconUrl} name={name} />
+      <UserItemBio>
+        <UserItemName>{name}</UserItemName>
+      </UserItemBio>
+    </UserItem>
+    <UserInteractiveItemActionGroup>{children}</UserInteractiveItemActionGroup>
+  </UserInteractiveItem>
+);
