1c5cb5e961633a48e29a02f004cc73c60384070f
ReoHakase <reonaldhkt+webs@gmail.com>
2022-10-06 20:07:44 +0900

    doc: 📝 (HttpError) HTTPエラー時にフォールバックされるページの見た目を担うコンポーネント`<HttpError>`のストーリーを作成した。

---
+++ src/features/httperrors/components/HttpError.story.tsx
@@ -0,0 +1,48 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import type { ComponentStoryObj, ComponentMeta } from '@storybook/react';
+
+import { HttpError } from './HttpError';
+
+type Story = ComponentStoryObj<typeof HttpError>;
+
+const meta: ComponentMeta<typeof HttpError> = {
+  component: HttpError,
+  args: {
+    code: '777',
+    description: 'Error Description',
+  },
+  parameters: {
+    layout: 'fullscreen',
+  },
+  argTypes: {
+    className: {
+      description: '`padding`等の調整のために公開されていますが、基本的には使用しないことが好ましいです。',
+      control: { type: 'text' },
+    },
+    code: {
+      description: '表示されるHTTPエラーコードを指定する。デザインの都合上、英字のみで指定することが好ましい。必須。',
+      control: { type: 'text' },
+    },
+    description: {
+      description: '表示されるHTTPエラーの説明を指定する。デザインの都合上、英字のみで指定することが好ましい。必須。',
+      control: { type: 'text' },
+    },
+  },
+};
+
+export default meta;
+
+export const Default: Story = {};
+
+export const Code404: Story = {
+  args: {
+    code: '404',
+    description: 'Not Found',
+  },
+};
+export const Code500: Story = {
+  args: {
+    code: '500',
+    description: 'Internal Server Error',
+  },
+};
