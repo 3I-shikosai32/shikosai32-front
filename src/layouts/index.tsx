00dfb40b381a021cedd532e616981001cf23ef57
ReoHakase <reonaldhkt+webs@gmail.com>
2022-10-06 21:15:21 +0900

    fix: 🐞 (Layout, Navigation) 上に重なる要素の`z-index`を引き上げるように修正

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -12,7 +12,7 @@ export type LayoutProps = ComponentPropsWithoutRef<'main'> &
 export const Layout: FC<LayoutProps> = ({ children, title, className, ...props }) => (
   <div className="m-0 flex min-h-screen w-full flex-col items-center justify-start">
     <CommonMeta title={title} />
-    <Header className="sticky top-0 left-0 z-30 m-0 w-full grow-0" />
+    <Header className="sticky top-0 left-0 z-50 m-0 w-full grow-0" />
     <main className={twMerge('m-0 flex w-full grow flex-col items-center justify-start', className)} {...props}>
       {children}
     </main>
