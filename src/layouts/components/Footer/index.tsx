b35eca67f4b95bfb6d659d499b8c9add08db7de8
ReoHakase <reonaldhkt+webs@gmail.com>
2022-10-06 20:11:41 +0900

    fix: 🐛 (Footer) フッター`<Footer>`記載のリンク先を正しいものに修正した。

--- src/layouts/components/Footer/index.tsx
+++ src/layouts/components/Footer/index.tsx
@@ -16,7 +16,7 @@ export const Footer: FC<FooterProps> = ({ className, ...props }) => (
       </figure>
       <ul className="flex flex-col items-start justify-end gap-1">
         <li>
-          <Link href="https://example.com" className="gap-2 font-normal text-neutral-400 drop-shadow-none">
+          <Link href="https://twitter.com/3i_shikosai32" className="gap-2 font-normal text-neutral-400 drop-shadow-none">
             <LinkIcon className="text-neutral-200">
               <FaTwitter />
             </LinkIcon>
@@ -24,7 +24,7 @@ export const Footer: FC<FooterProps> = ({ className, ...props }) => (
           </Link>
         </li>
         <li>
-          <Link href="https://example.com" className="gap-2 font-normal text-neutral-400 drop-shadow-none">
+          <Link href="https://www.shikosai.net/" className="gap-2 font-normal text-neutral-400 drop-shadow-none">
             <LinkIcon className="text-neutral-200">
               <MdFestival />
             </LinkIcon>
@@ -32,7 +32,7 @@ export const Footer: FC<FooterProps> = ({ className, ...props }) => (
           </Link>
         </li>
         <li>
-          <Link href="https://example.com" className="gap-2 font-normal text-neutral-400 drop-shadow-none">
+          <Link href="https://www.ibaraki-ct.ac.jp/" className="gap-2 font-normal text-neutral-400 drop-shadow-none">
             <LinkIcon className="text-neutral-200">
               <RiGlobalFill />
             </LinkIcon>
