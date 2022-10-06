777411e4a6e07d107f71820927eddd7183949a2f
ReoHakase <reonaldhkt+webs@gmail.com>
2022-10-06 20:11:07 +0900

    feat: ✨ (src/pages/) 仮のLPをルーティングに追加した。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 import type { NextPage } from 'next';
-import Index from '@/features/index';
+import { TemporalIndex } from '@/features/index/TemporalIndex';
+import { Layout } from '@/layouts';
 
-const IndexPage: NextPage = () => <Index />;
+const IndexPage: NextPage = () => (
+  <Layout title="OZ: 第32回茨香祭で開催予定！">
+    <TemporalIndex />
+  </Layout>
+);
 
 export default IndexPage;
