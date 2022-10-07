0a80879403bb3365d6302a34620e3b5b98fe9c39
ReoHakase <reonaldhkt+webs@gmail.com>
2022-10-08 06:02:51 +0900

    wip: 🚧 (Toggle) WIP
    措定していたものよりも変更箇所が多くなりそうなので一旦保留にします

--- src/components/Toggle/Toggle.story.tsx
+++ src/components/Toggle/Toggle.story.tsx
@@ -1,37 +1,42 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { action } from '@storybook/addon-actions';
 import type { ComponentStoryObj, ComponentMeta } from '@storybook/react';
-import Toggle from ".";
-
+import Toggle from '.';
 
 type Story = ComponentStoryObj<typeof Toggle>;
 
 const meta: ComponentMeta<typeof Toggle> = {
-	component: Toggle,
-	args:{
-		onChange: action('onChange'),
-	},
-	argTypes:{
-		color:{
-			description:'bg-gray-500のようにして指定する'
-		},
-		size:{
-			description:'small,mid,largeから選択する',
-			options: ['small', 'mid', 'large'],
-            control: { type: 'radio' },
-		},
-		checked:{
-			description:'small,mid,largeから選択する',
-			control: {type:'boolean'},
-		}
-	}
-}
-
+  component: Toggle,
+  args: {
+    onChange: action('onChange'),
+    className: 'bg-neutral-500',
+  },
+  argTypes: {
+    id: {
+      description: 'HTML標準。ラベルと紐付けるためのIDを指定する。アクセシビリティのために必須。',
+      control: { type: 'text' },
+    },
+    onChange: {
+      description: 'HTML標準。トグル`<input type="checkbox">`の状態が変更されたときに呼び出されるイベントハンドラを指定する。',
+      control: { type: 'none' },
+    },
+    checked: {
+      description: 'HTML標準。トグル`<input type="checkbox">`の状態を指定する。',
+      control: { type: 'boolean' },
+    },
+    className: {
+      description: 'トグル`<input type="checkbox">`のスタイルを変更するために用意されている。',
+      control: { type: 'text' },
+    },
+  },
+};
 
 export default meta;
 
-export const Default: Story = {
-	args: {
+export const Default: Story = {};
 
-	},
-  };
+export const WithSmallSize: Story = {
+  args: {
+    className: 'bg-neutral-500 h-6',
+  },
+};
