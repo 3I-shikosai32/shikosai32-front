f3870818b0eaf5569b6c4faa2b95fcabf8e401ec
ReoHakase <reonaldhkt+webs@gmail.com>
2022-10-08 03:15:00 +0900

    refactor: ♻️ (AudioControlMenu) 現在の再生時間の表示の再レンダリング回数を`useMemo`を使い軽減するようにした。

--- src/layouts/components/AudioControlMenu/index.tsx
+++ src/layouts/components/AudioControlMenu/index.tsx
@@ -1,4 +1,5 @@
 import type { FC } from 'react';
+import { useMemo } from 'react';
 import { BsPauseFill, BsPlayFill } from 'react-icons/bs';
 import { HiMusicNote } from 'react-icons/hi';
 import { formatDuration } from './formatDuration';
@@ -29,48 +30,51 @@ export const AudioControlMenu: FC<AudioControlMenuProps> = ({
   viewportClassName,
   className,
   ...props
-}) => (
-  <NavigationMenu viewportClassName={twMerge('left-0 justify-start min-w-[24rem]', viewportClassName)}>
-    <NavigationItem>
-      <NavigationTrigger>
-        <Button className={twMerge('p-0 min-h-12 ', className)} ghost circle aria-label="音楽プレイヤーを開く" {...props}>
-          {/* TODO: ここに音楽の波形に合わせたスペクトラムを挿入する */}
-          <ButtonIcon>
-            <HiMusicNote />
-          </ButtonIcon>
-        </Button>
-      </NavigationTrigger>
-      <NavigationContent className="w-auto items-center gap-2 p-6">
-        <div className={twMerge('flex flex-row items-baseline justify-between gap-4', composers.length > 1 && 'items-center')}>
-          <h1 className="font-branding text-3xl font-bold">{name}</h1>
-          <div className="flex flex-col-reverse items-start justify-start gap-0">
-            {composers.map((composer) => (
-              <NavigationLink key={composer.name}>
-                <Link className="font-normal text-neutral-500" href={composer.social || '#'}>
-                  {composer.name}
-                </Link>
-              </NavigationLink>
-            ))}
-          </div>
-        </div>
-        <p className="text-xs text-neutral-500">{`${formatDuration(currentTime)} / ${formatDuration(duration)}`}</p>
-        <div>
-          <Button
-            className="text-neutral-500"
-            onClick={() => {
-              if (isPlaying) {
-                onPause();
-              } else {
-                onPlay();
-              }
-            }}
-            circle
-            outlined
-          >
-            <ButtonIcon>{isPlaying ? <BsPauseFill /> : <BsPlayFill />}</ButtonIcon>
+}) => {
+  const formattedTimeDisplay = useMemo(() => `${formatDuration(currentTime)} / ${formatDuration(duration)}`, [currentTime, duration]);
+  return (
+    <NavigationMenu viewportClassName={twMerge('left-0 justify-start min-w-[24rem]', viewportClassName)}>
+      <NavigationItem>
+        <NavigationTrigger>
+          <Button className={twMerge('p-0 min-h-12 ', className)} ghost circle aria-label="音楽プレイヤーを開く" {...props}>
+            {/* TODO: ここに音楽の波形に合わせたスペクトラムを挿入する */}
+            <ButtonIcon>
+              <HiMusicNote />
+            </ButtonIcon>
           </Button>
-        </div>
-      </NavigationContent>
-    </NavigationItem>
-  </NavigationMenu>
-);
+        </NavigationTrigger>
+        <NavigationContent className="w-auto items-center gap-2 p-6">
+          <div className={twMerge('flex flex-row items-baseline justify-between gap-4', composers.length > 1 && 'items-center')}>
+            <h1 className="font-branding text-3xl font-bold">{name}</h1>
+            <div className="flex flex-col-reverse items-start justify-start gap-0">
+              {composers.map((composer) => (
+                <NavigationLink key={composer.name}>
+                  <Link className="font-normal text-neutral-500" href={composer.social || '#'}>
+                    {composer.name}
+                  </Link>
+                </NavigationLink>
+              ))}
+            </div>
+          </div>
+          <p className="text-xs text-neutral-500">{formattedTimeDisplay}</p>
+          <div>
+            <Button
+              className="text-neutral-500"
+              onClick={() => {
+                if (isPlaying) {
+                  onPause();
+                } else {
+                  onPlay();
+                }
+              }}
+              circle
+              outlined
+            >
+              <ButtonIcon>{isPlaying ? <BsPauseFill /> : <BsPlayFill />}</ButtonIcon>
+            </Button>
+          </div>
+        </NavigationContent>
+      </NavigationItem>
+    </NavigationMenu>
+  );
+};
