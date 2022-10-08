2b612d5649277736156b6c9ec6fc049085a1365d
ReoHakase <reonaldhkt+webs@gmail.com>
2022-10-09 00:59:37 +0900

    refactor: ♻️ (GiftTransactionItem) GraphQLのスキーマ由来の型定義に依存しないよう変更した。
    代わりに、外部に依存しない型定義`src/entity`を使用するようになった。追加し忘れを追加した。

--- src/features/history/components/GiftTransactionItem/index.tsx
+++ src/features/history/components/GiftTransactionItem/index.tsx
@@ -1,17 +1,14 @@
 import type { FC } from 'react';
 import { IoMdSwap } from 'react-icons/io';
 import { TransactionItem, TransactionItemProps } from '@/components/TransactionItem';
-import type { GiftHistoryDataFragment } from '@/infra/graphql/generated/graphql';
+import type { GiftTransaction } from '@/entity/Transaction';
 
-export type GiftTransactionItemData = Pick<GiftHistoryDataFragment, 'id' | 'isDelivered' | 'createdAt' | 'deliveredAt'> & {
-  user: Pick<GiftHistoryDataFragment['user'], 'name' | 'iconUrl'>;
-  exchangedGift: Pick<GiftHistoryDataFragment['exchangedGift'], 'name'>;
-};
+export type GiftTransactionItemData = GiftTransaction;
 
 export type GiftTransactionItemProps = Omit<TransactionItemProps, 'children'> & GiftTransactionItemData;
-export const GiftTransactionItem: FC<GiftTransactionItemProps> = ({ exchangedGift, ...props }) => (
-  <TransactionItem {...props}>
+export const GiftTransactionItem: FC<GiftTransactionItemProps> = ({ exchangedItem, ...props }) => (
+  <TransactionItem {...{ exchangedItem, ...props }}>
     <IoMdSwap className="text-lg text-neutral-500" />
-    {exchangedGift.name}
+    {exchangedItem.name}
   </TransactionItem>
 );
