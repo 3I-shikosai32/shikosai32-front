daeff41f7b2c0c912230ffab9b111cad7e1b1611
ReoHakase <reonaldhkt+webs@gmail.com>
2022-10-09 23:50:19 +0900

    fix: 🐞 (src/state/audio) 型のインポート元を新たな`src/model/audio`からのものに変更した。

--- src/state/audio/audio-resource.spec.tsx
+++ src/state/audio/audio-resource.spec.tsx
@@ -1,5 +1,5 @@
 import { resolveAudioResource } from './audio-resource';
-import type { AudioResource } from './audio-resource';
+import type { AudioResource } from '@/model/audio/audio-resource.model';
 
 const audioResources: Array<AudioResource> = [
   {
