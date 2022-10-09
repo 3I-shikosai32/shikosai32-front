2e0d9d6206f929709175ba6c2f7aab7247be2e06
ReoHakase <reonaldhkt+webs@gmail.com>
2022-10-09 21:54:33 +0900

    refactor: ♻️ (src/utils/characterDictionary) `Character`モデルに結びついた定数を提供する`src/utils/characterDictionary`の内容を`src/model/character.model.ts`に移した。

--- src/model/character.model.ts
+++ src/model/character.model.ts
@@ -6,3 +6,21 @@ export enum Character {
   Reaper = 'REAPER',
   Tree = 'TREE',
 }
+
+export const CharacterNameDictionary: Record<Character, string> = {
+  [Character.Fox]: 'きゅうびさん',
+  [Character.Cat]: 'ねこさん',
+  [Character.Pudding]: 'あらもーちゃん',
+  [Character.Reaper]: 'りっぱーさん',
+  [Character.Tree]: 'おおもりさん',
+  [Character.Goku]: 'ごくうさん',
+};
+
+export const CharacterIconUrlDictionary: Record<Character, string> = {
+  [Character.Fox]: '/icons/fox.png',
+  [Character.Cat]: '/icons/cat.png',
+  [Character.Pudding]: '/icons/pudding.png',
+  [Character.Reaper]: '/icons/reaper.png',
+  [Character.Tree]: '/icons/tree.png',
+  [Character.Goku]: '/icons/goku.png',
+};
