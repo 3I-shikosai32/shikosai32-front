96b1acecf0d9ff4f98f5bcfa66f9776e40b6a4fb
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-11 10:16:30 +0900

    fix: 🐛 (new-user ) レスポンシブの修正

--- src/features/new-user/components/character.tsx
+++ src/features/new-user/components/character.tsx
@@ -11,12 +11,14 @@ type CharacterProps = {
 const Character: FC<CharacterProps> = ({ checkedItems, handleChange }) => (
   <div className=" grid grid-cols-3 px-5">
     {Object.values(Charac).map((character) => (
-      <div key={character} className="my-5 mx-3  h-[150px] w-[140px] rounded-2xl bg-white shadow-2xl md:mx-10 md:h-[200px] md:w-[200px]">
-        <div className="top-5 m-1 md:m-3">
+      <div key={character} className="my-5 mx-3  h-[110px] w-[90px] rounded-2xl bg-white shadow-2xl md:mx-10 md:h-[200px] md:w-[200px]">
+        <div className="top-5 m-1  md:m-3">
           <input id={character} type="checkbox" className="h-5 w-5" checked={checkedItems === character} onChange={handleChange} />
         </div>
         <div className="flex justify-center">
-          <Image src={`/characters/${character.toLowerCase()}.svg`} width={110} height={110} alt="My icon" />
+          <div className='h-[110px] w-[110px] translate-y-[-10px] p-[10px] md:translate-y-[0px] md:p-[0px]'>
+            <Image src={`/characters/${character.toLowerCase()}.svg`} width={110} height={110} alt="My icon" layout='responsive' />
+          </div>
         </div>
       </div>
     ))}
