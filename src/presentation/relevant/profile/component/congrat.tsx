0c52136a2dc2c5b865a5782ec9ffcd2a269be2e5
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-14 00:10:55 +0900

    feat: ✨ (profile) Congratコンポーネントの作成

---
+++ src/presentation/relevant/profile/component/congrat.tsx
@@ -0,0 +1,13 @@
+import type { FC } from "react";
+
+const Congrat: FC = () => (
+	<div className="mx-[10px] mt-[100px] w-[95%]">
+		<div className="flex justify-center rounded-lg border border-primary bg-primary-100 px-[20px]">
+			<div>
+				<div className="text-center text-3xl ">おめでとう!</div>
+				<div className="mt-[10px]">4つの着せ替えアイテムを集めたあなたは、特別に缶バッジを受け取ることが出来ます。景品受取所でこの画面を見せて、缶バッジをもらおう</div>
+			</div>
+		</div>
+	</div>
+)
+export default Congrat
