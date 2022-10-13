939b63ef1cd87549320a0529f1398f311f3952c5
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-14 00:11:23 +0900

    feat: ✨ (profile) キャラクターコンポーネントの作成

---
+++ src/presentation/relevant/profile/component/character.tsx
@@ -0,0 +1,12 @@
+import type { FC } from "react";
+import { Icon } from "@/presentation/primitive/component/icon/icon.presenter";
+
+const Character: FC = () => (
+	<div className="w-full">
+		<div className="flex justify-center bg-character-fox-g1">
+			<Icon src="/icons/cat.png" className="h-[150px] w-[150px] translate-y-10 p-10" />
+			<div className="ml-[20px] translate-y-[70px] text-6xl text-white">Dino3161</div>
+		</div>
+	</div>
+)
+export default Character
