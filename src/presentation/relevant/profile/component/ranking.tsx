3a95bcc312702eb2a7b96a34ea9220c2d7f75d7a
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-14 00:09:41 +0900

    feat: ✨ (profile_page) rankingコンポーネントの作成

---
+++ src/presentation/relevant/profile/component/ranking.tsx
@@ -0,0 +1,24 @@
+import type { FC } from "react";
+import { Button } from "@/presentation/primitive/component/button/button.presenter";
+import { Icon } from "@/presentation/primitive/component/icon/icon.presenter";
+
+const Ranking: FC = () => (
+	<div className="mx-[15px] mt-[25px] w-[95%]">
+		<div className="rounded-lg bg-white px-[20px] pt-[15px] pb-[5px] shadow-xl">
+			<div className="">
+				<div className="text-center text-3xl ">あなたの順位</div>
+				<div className="text-center text-sm ">全参加者128人の中で...</div>
+				<div className="mb-[50px] flex translate-y-[-20px] justify-center">
+					<div className="mr-[10px] translate-y-[55px] text-5xl">#1</div>
+					<Icon src="/icons/cat.png" className="h-[10px] w-[20px] translate-y-10 p-10" />
+					<div className="ml-[10px] translate-y-[60px] text-center">
+						<div>57Ptをわが物とし栄冠を載するのは...</div>
+						<div className="text-3xl">Dino316</div>
+					</div>
+				</div>
+			</div>
+			<div className="my-[10px] flex justify-center"><Button className="bg-gradient-to-r from-twitter-g1 to-twitter-g2">ツイートする</Button></div>
+		</div>
+	</div>
+)
+export default Ranking
