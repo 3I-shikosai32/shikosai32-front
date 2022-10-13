f66111a172bdae28bdec6efe07604b99e6a94aa9
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-14 05:20:25 +0900

    feat: ✨ (relevant/is-new-user) IsNewUserを作成

---
+++ src/presentation/relevant/is-new-user/is-new-user.page.tsx
@@ -0,0 +1,16 @@
+import type { FC } from 'react';
+import { di } from 'react-magnetic-di';
+import { HttpError500 } from '../../http-error/http-error-500.page';
+import { NewUserDetectorContainer } from './component/new-user-detector.container';
+import { useCurrentUserIdUseCase } from '@/use-case/user/use-current-user-id.use-case';
+
+export const IsNewUser: FC = () => {
+  di(useCurrentUserIdUseCase);
+
+  const user = useCurrentUserIdUseCase();
+  if (user === null) {
+    return <HttpError500 />;
+  }
+
+  return <NewUserDetectorContainer id={user.id} />;
+};
