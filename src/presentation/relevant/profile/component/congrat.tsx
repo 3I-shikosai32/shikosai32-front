81affdac1b3129f1de52080da77ad9af7095aeae
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-15 03:00:19 +0900

    fix: 🐛 (profile) フォントの修正

--- src/presentation/relevant/profile/component/congrat.tsx
+++ src/presentation/relevant/profile/component/congrat.tsx
@@ -2,10 +2,10 @@ import type { FC } from "react";
 
 const Congrat: FC = () => (
 	<div className="mx-[10px] mt-[100px] w-[95%]">
-		<div className="flex justify-center rounded-lg border border-primary bg-primary-100 px-[20px]">
-			<div>
-				<div className="text-center text-3xl ">おめでとう!</div>
-				<div className="mt-[10px]">4つの着せ替えアイテムを集めたあなたは、特別に缶バッジを受け取ることが出来ます。景品受取所でこの画面を見せて、缶バッジをもらおう</div>
+		<div className="flex justify-center rounded-3xl border border-primary bg-primary-100 px-[20px]">
+			<div className="font-pixel">
+				<div className="text-center text-[54px]">おめでとう!</div>
+				<div className="mt-[10px] text-[27px] text-primary-700">4つの着せ替えアイテムを集めたあなたは、特別に缶バッジを受け取ることが出来ます。景品受取所でこの画面を見せて、缶バッジをもらおう</div>
 			</div>
 		</div>
 	</div>
