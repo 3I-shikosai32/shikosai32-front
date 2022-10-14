d4bb3785c6aeab561a2dba419cbe8ad403ac13c6
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-15 03:00:40 +0900

    fix: 🐛 (profile) 景品交換のアイコンを作成

--- src/presentation/relevant/profile/component/point.tsx
+++ src/presentation/relevant/profile/component/point.tsx
@@ -1,18 +1,27 @@
 import type { FC } from "react";
-import { Button } from "@/presentation/primitive/component/button/button.presenter";
+import { BsFillGiftFill } from "react-icons/bs";
+import { Button, ButtonIcon } from "@/presentation/primitive/component/button/button.presenter";
+
 
 const Point: FC = () => (
-	<div className="mx-[10px] mt-[25px] w-[95%] bg-white shadow-xl">
-		<div className="flex justify-center rounded-lg px-[20px] ">
+	<div className="mx-[10px] mt-[25px] w-[95%] rounded-3xl bg-white shadow-xl">
+		<div className="flex justify-center px-[20px] ">
 			<div>
-				<div className="text-center text-3xl ">残り消費Pt</div>
+				<div className="text-center text-[40px] font-bold">残り消費Pt</div>
 				<div className="text-center text-3xl">
 					<span className="text-5xl">34</span>
 					<span>pt</span>
 				</div>
 			</div>
 		</div>
-		<div className="my-[10px] flex justify-center"><Button className="bg-gradient-to-r from-twitter-g1 to-twitter-g2">景品を交換する</Button></div>
+		<div className="my-[10px] flex justify-center">
+			<Button className="bg-exchange">
+				<ButtonIcon>
+					<BsFillGiftFill />
+				</ButtonIcon>
+				<i>景品を交換する</i>
+			</Button>
+		</div>
 	</div>
 )
 export default Point
