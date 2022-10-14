a6450e7828fbe29b9a3f4ea4b255b1cf3c6b1f7d
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-15 02:59:08 +0900

    fix: 🐛 (profile) twitterアイコンの修正

--- src/presentation/relevant/profile/component/ranking.tsx
+++ src/presentation/relevant/profile/component/ranking.tsx
@@ -1,15 +1,16 @@
 import type { FC } from "react";
-import { Button } from "@/presentation/primitive/component/button/button.presenter";
+import { RiTwitterLine } from "react-icons/ri";
+import { Button, ButtonIcon } from "@/presentation/primitive/component/button/button.presenter";
 import { Icon } from "@/presentation/primitive/component/icon/icon.presenter";
 
 const Ranking: FC = () => (
 	<div className="mx-[15px] mt-[25px] w-[95%]">
-		<div className="rounded-lg bg-white px-[20px] pt-[15px] pb-[5px] shadow-xl">
+		<div className="rounded-3xl bg-white px-[20px] pt-[15px] pb-[5px] shadow-xl">
 			<div className="">
-				<div className="text-center text-3xl ">あなたの順位</div>
+				<div className="text-center font-body text-[40px] font-bold">あなたの順位</div>
 				<div className="text-center text-sm ">全参加者128人の中で...</div>
 				<div className="mb-[50px] flex translate-y-[-20px] justify-center">
-					<div className="mr-[10px] translate-y-[55px] text-5xl">#1</div>
+					<div className="mr-[10px] translate-y-[55px] text-5xl text-gold">#1</div>
 					<Icon src="/icons/cat.png" className="h-[10px] w-[20px] translate-y-10 p-10" />
 					<div className="ml-[10px] translate-y-[60px] text-center">
 						<div>57Ptをわが物とし栄冠を載するのは...</div>
@@ -17,7 +18,16 @@ const Ranking: FC = () => (
 					</div>
 				</div>
 			</div>
-			<div className="my-[10px] flex justify-center"><Button className="bg-gradient-to-r from-twitter-g1 to-twitter-g2">ツイートする</Button></div>
+			<div className="my-[10px] flex justify-center">
+				<Button className="bg-gradient-to-r from-twitter-g1 to-twitter-g2">
+					<ButtonIcon>
+						<RiTwitterLine />
+					</ButtonIcon>
+					<i>
+						ツイートする
+					</i>
+				</Button>
+			</div>
 		</div>
 	</div>
 )
