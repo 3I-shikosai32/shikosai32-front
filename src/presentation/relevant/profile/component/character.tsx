854059dc210eb6c91e0410559b33983d6b16123b
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-15 02:59:38 +0900

    fix: 🐛 (profile) キャラクターアイコンの背景をグラデーションに変更

--- src/presentation/relevant/profile/component/character.tsx
+++ src/presentation/relevant/profile/component/character.tsx
@@ -3,7 +3,7 @@ import { Icon } from "@/presentation/primitive/component/icon/icon.presenter";
 
 const Character: FC = () => (
 	<div className="w-full">
-		<div className="flex justify-center bg-character-fox-g1">
+		<div className="flex justify-center bg-gradient-to-r from-character-fox-g1 to-character-fox-g2">
 			<Icon src="/icons/cat.png" className="h-[150px] w-[150px] translate-y-10 p-10" />
 			<div className="ml-[20px] translate-y-[70px] text-6xl text-white">Dino3161</div>
 		</div>
