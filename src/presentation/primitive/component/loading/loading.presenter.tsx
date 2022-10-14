b37240e401e5d0285e2b0e1991cabb57fbc5956e
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-14 15:39:37 +0900

    fix: 🐞 (Loading) HTML divを継承し、defaultPropsを削除

--- src/presentation/primitive/component/loading/loading.presenter.tsx
+++ src/presentation/primitive/component/loading/loading.presenter.tsx
@@ -1,14 +1,12 @@
-import type { FC } from 'react';
+import type { ComponentPropsWithoutRef, FC } from 'react';
 import twMerge from '@/presentation/style/twmerge';
 
-const Loading: FC<{ className?: string }> = ({ className }) => (
+export type LoadingProps = Pick<ComponentPropsWithoutRef<'div'>, 'className'>;
+
+const Loading: FC<LoadingProps> = ({ className }) => (
   <div className="flex justify-center">
     <div className={twMerge('animate-spin rounded-full border-4 border-pink-500 border-t-transparent', className || 'h-36 aspect-square')} />
   </div>
 );
-
-Loading.defaultProps = {
-  className: undefined,
-};
 
 export default Loading;
