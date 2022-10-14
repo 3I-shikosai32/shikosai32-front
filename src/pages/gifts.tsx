c2d3282271feeb05f21a4cd2730da35879436aec
ReoHakase <reonaldhkt+webs@gmail.com>
2022-10-15 01:20:55 +0900

    fix: 🐞 (GiftIndex) ルーティング`src/pages/gifts.tsx`で使用する`import`元のパスを、ファイル名をコンポーネント名に合うものに修正されたものに変更した。

--- src/pages/gifts.tsx
+++ src/pages/gifts.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import { Layout } from '@/presentation/layout/layout.container';
-import { GiftIndexPage } from '@/presentation/relevant/gift/gift.page';
+import { GiftIndexPage } from '@/presentation/relevant/gift/gift-index.page';
 
 const IndexPage: NextPage = () => (
   <Layout title="OZ | 景品交換">
