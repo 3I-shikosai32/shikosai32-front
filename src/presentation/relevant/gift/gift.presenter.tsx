0cf95a883c15bf26e9582402ca46c6d01bd9a31c
ReoHakase <reonaldhkt+webs@gmail.com>
2022-10-15 01:13:40 +0900

    feat: ✨ (GiftIndex (Presenter)) 景品交換ページの見た目を担うPresenterコンポーネントの`<GiftIndex>`を作成した。

---
+++ src/presentation/relevant/gift/gift.presenter.tsx
@@ -0,0 +1,81 @@
+import type { FC } from 'react';
+import { GiftItem } from './component/gift-item/gift-item.presenter';
+import type { GiftItemProps } from './component/gift-item/gift-item.presenter';
+
+import type { Gift } from '@/model/gift/gift.model';
+import { Card } from '@/presentation/primitive/component/card/card.presenter';
+
+export type GiftIndexProps = {
+  gifts: Array<Pick<Gift, 'id' | 'iconUrl' | 'name' | 'price' | 'remaining'>> | undefined | null;
+  consumablePoint?: number | undefined | null;
+  onExchange: GiftItemProps['onExchange'];
+};
+
+const resolvePlaceholderMessage = (gifts: GiftIndexProps['gifts']): string => {
+  if (gifts === undefined) {
+    return '読み込み中です...';
+  }
+  if (gifts === null) {
+    return '読み込み中にエラーが発生しました';
+  }
+  if (gifts.length < 1) {
+    return '景品がみつかりませんでした';
+  }
+  return '';
+};
+
+export const GiftIndex: FC<GiftIndexProps> = ({ gifts, consumablePoint, onExchange }) => (
+  <div className="flex flex-col items-center justify-start gap-4">
+    <div className="m-0 -mb-20 flex w-screen flex-col items-center justify-start gap-2 bg-gradient-to-br p-4 py-12 pb-24 text-center text-white gradient-exchange">
+      <h1 className="mb-4 font-branding text-5xl font-bold">Exchange</h1>
+      {consumablePoint !== undefined && consumablePoint !== null ? (
+        <>
+          <p>
+            あなたが獲得したポイントと、
+            <br />
+            景品を交換しましょう！
+          </p>
+          <span className="inline-flex items-baseline justify-center gap-1 font-branding font-bold">
+            <h2 className="text-8xl">{consumablePoint}</h2>
+            Pt
+          </span>
+          <span className="text-xs">が使用可能です</span>
+        </>
+      ) : (
+        <>
+          <p>
+            ゲームに参加してもらえるポイントを使って、
+            <br />
+            現実世界の景品と交換することができます！
+            <br />
+          </p>
+          <div className="m-2 rounded-base border-2 border-white p-4 font-pixel">
+            画面上部のユーザーアイコンから
+            <br />
+            登録・ログインして、
+            <br />
+            あなたも景品のために遊ぼう！
+          </div>
+        </>
+      )}
+    </div>
+    <Card className="mx-4 grid grid-flow-row grid-cols-1 justify-center gap-4 gap-x-8 p-2 lg:grid-cols-2">
+      {gifts && gifts.map((gift) => <GiftItem key={gift.id} {...gift} consumablePoint={consumablePoint || 0} onExchange={onExchange} />)}
+      {(!gifts || gifts === null || gifts.length < 1) && (
+        <GiftItem
+          id="0"
+          className="col-span-full"
+          remaining={0}
+          price={1}
+          name={resolvePlaceholderMessage(gifts)}
+          consumablePoint={1}
+          onExchange={() => {}}
+        />
+      )}
+    </Card>
+  </div>
+);
+
+GiftIndex.defaultProps = {
+  consumablePoint: undefined,
+};
