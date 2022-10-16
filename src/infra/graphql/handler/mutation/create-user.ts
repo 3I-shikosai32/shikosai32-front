a9959daf0fe6346a9f1c9dc458d187750bd2f913
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-10-16 10:24:07 +0900

    refactor: ♻️ new-userのリファクタリング時に不要になるファイルを削除

--- src/infra/graphql/handler/mutation/create-user.ts
+++
@@ -1,9 +0,0 @@
-import { useCreateUserMutation } from "../../generated/graphql";
-
-const useCreateUserMutationHandler = () => {
-	const [{ data, fetching }, executeMutation] = useCreateUserMutation();
-  
-	return { data, fetching, executeMutation };
-  };
-  
-  export default useCreateUserMutationHandler;
