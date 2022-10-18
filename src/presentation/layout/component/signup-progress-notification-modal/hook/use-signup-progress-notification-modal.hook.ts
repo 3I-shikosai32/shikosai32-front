c94d26b06ad527def04d44388299b05314dcc166
ReoHakase <reonaldhkt+webs@gmail.com>
2022-10-19 04:54:01 +0900

    refactor: ♻️ (Auth, SignupProgressNotificationModal) 現在のユーザーのFirebaseでの認証状態とDBでのユーザー情報の登録の有無を調べるユースケースフック`useCurrentUserAuthenticationStatusUseCase`を使うように変更した。
    そのユースケースフックは通していたコードをまとめたものだから。

--- src/presentation/layout/component/signup-progress-notification-modal/hook/use-signup-progress-notification-modal.hook.ts
+++ src/presentation/layout/component/signup-progress-notification-modal/hook/use-signup-progress-notification-modal.hook.ts
@@ -1,8 +1,7 @@
 import { useRouter } from 'next/router';
 import { useState, useMemo, useEffect } from 'react';
 import type { SignupProgressNotificationModalProps } from '../signup-progress-notification-modal.presenter';
-import { useCheckUserExistanceUseCase } from '@/use-case/user/use-check-user-existance.use-case';
-import { useCurrentUserIdUseCase } from '@/use-case/user/use-current-user-id.use-case';
+import { useCurrentUserAuthenticationStatusUseCase } from '@/use-case/user/use-current-user-authentication-status.use-case';
 
 const authPagePathnameRegExp = /^\/auth(\/.+|)/; // `/auth`以下のすべてのパスにマッチする正規表現
 
@@ -18,12 +17,8 @@ export const useSignupProgressNotificationModal = (): Pick<SignupProgressNotific
 
   const { pathname } = useRouter();
   const isCurrentlyAtAuthPage = useMemo(() => authPagePathnameRegExp.test(pathname), [pathname]);
-  const currentUser = useCurrentUserIdUseCase(); // Firebase Authの認証情報由来のid
-  const hasUserAuthenticated = useMemo(() => !!currentUser, [currentUser]); // ユーザーがFirebase Authでログイン済みかどうか
-  // ユーザーが各種情報を登録済みかどうか
-  const hasUserRegisteredInfo = useCheckUserExistanceUseCase({
-    id: currentUser?.id,
-  });
+  const { hasUserAuthenticated, hasUserRegisteredInfo } = useCurrentUserAuthenticationStatusUseCase();
+
   const shouldShowModal = useMemo(() => {
     if (!hasComponentMounted) return false; // Hydrationが失敗しないようにするため
     return hasUserAuthenticated && !hasUserRegisteredInfo && !isCurrentlyAtAuthPage;
--- src/presentation/relevant/auth/auth.page.tsx
+++ src/presentation/relevant/auth/auth.page.tsx
@@ -8,18 +8,12 @@ import { Card } from '../../primitive/component/card/card.presenter';
 import { Link, LinkIcon } from '../../primitive/component/link/link.presenter';
 import { loginWithGoogle } from '@/infra/firebase/auth';
 import { Modal, ModalTitle, ModalDescription, ModalOverlay, ModalContent } from '@/presentation/primitive/component/modal/modal.presenter';
-import { useCheckUserExistanceUseCase } from '@/use-case/user/use-check-user-existance.use-case';
-import { useCurrentUserIdUseCase } from '@/use-case/user/use-current-user-id.use-case';
+import { useCurrentUserAuthenticationStatusUseCase } from '@/use-case/user/use-current-user-authentication-status.use-case';
 
 export const Auth: FC = () => {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
   const [shouldShowErrorModal, setShouldShowErrorModal] = useState(false);
-  const currentUser = useCurrentUserIdUseCase(); // Firebase Authの認証情報由来のid
-  const hasUserAuthenticated = useMemo(() => !!currentUser, [currentUser]); // ユーザーがFirebase Authでログイン済みかどうか
-  // ユーザーが各種情報を登録済みかどうか
-  const hasUserRegisteredInfo = useCheckUserExistanceUseCase({
-    id: currentUser?.id,
-  });
+  const { hasUserAuthenticated, hasUserRegisteredInfo } = useCurrentUserAuthenticationStatusUseCase();
   useEffect(() => {
     if (hasUserAuthenticated) {
       if (hasUserRegisteredInfo) {
