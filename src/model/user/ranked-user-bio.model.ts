91b339ac310b0ea4d707c67d37cdf1545d709d27
ReoHakase <reonaldhkt+webs@gmail.com>
2022-10-19 18:19:13 +0900

    refactor: ♻️ (Model RankedUserBio) ポイントのメンバを任意にした。

--- src/model/user/ranked-user-bio.model.ts
+++ src/model/user/ranked-user-bio.model.ts
@@ -2,5 +2,5 @@ import type { UserBio } from './user-bio.model';
 
 export type RankedUserBio = UserBio & {
   place: number; // 特定のカテゴリ、時間に依存しないその文脈での順位
-  point: number; // 特定のカテゴリ、時間に依存しないその文脈でのポイント
+  point?: number; // 特定のカテゴリ、時間に依存しないその文脈でのポイント
 };
