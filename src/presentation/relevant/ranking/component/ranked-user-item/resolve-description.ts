ce24229371c912cfc4d96ce0411ddbb9598942c1
ReoHakase <reonaldhkt+webs@gmail.com>
2022-10-20 05:34:37 +0900

    fix: 🐛 (RankedUserItem resolveDescription) ポイントが0 Ptのときでもポイントを表示できるように修正した。

--- src/presentation/relevant/ranking/component/ranked-user-item/resolve-description.ts
+++ src/presentation/relevant/ranking/component/ranked-user-item/resolve-description.ts
@@ -3,10 +3,10 @@ import type { RankedUserBio } from '@/model/user/ranked-user-bio.model';
 type ResolveDescriptionProps = Pick<RankedUserBio, 'place' | 'point'>;
 
 type Resolver = (point: ResolveDescriptionProps['point']) => string | null;
-const firstPlaceResolver: Resolver = (point) => (point ? `${point} Pt を我が物とし栄冠を戴くのは...` : `栄冠を戴くのは...`);
-const secondPlaceResolver: Resolver = (point) => (point ? `${point} Pt を勝ち取り、異彩を解き放つ！` : `異彩を解き放つ！`);
-const thirdPlaceResolver: Resolver = (point) => (point ? `${point} Pt を集めた遊びの達人` : `遊びの達人`);
-const defaultResolver: Resolver = (point) => (point ? `${point} Pt` : null);
+const firstPlaceResolver: Resolver = (point) => (typeof point === 'number' ? `${point} Pt を我が物とし栄冠を戴くのは...` : `栄冠を戴くのは...`);
+const secondPlaceResolver: Resolver = (point) => (typeof point === 'number' ? `${point} Pt を勝ち取り、異彩を解き放つ！` : `異彩を解き放つ！`);
+const thirdPlaceResolver: Resolver = (point) => (typeof point === 'number' ? `${point} Pt を集めた遊びの達人` : `遊びの達人`);
+const defaultResolver: Resolver = (point) => (typeof point === 'number' ? `${point} Pt` : null);
 
 export const resolveDescription = ({ place, point }: ResolveDescriptionProps): string | null => {
   switch (place) {
