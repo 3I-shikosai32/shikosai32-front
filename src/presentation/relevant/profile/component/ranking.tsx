613930bffad7ff45046a28e56a23672546babb2a
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-21 02:41:49 +0900

    fix: 🐛 (new-profile) ツイートボタンの修正

--- src/presentation/relevant/profile/component/ranking.tsx
+++ src/presentation/relevant/profile/component/ranking.tsx
@@ -1,8 +1,6 @@
 import type { FC } from "react";
-import { RiTwitterLine } from "react-icons/ri";
-import { TwitterShareButton } from "react-share"
+import { TwitterShareButton, TwitterIcon } from "react-share"
 import type { Character } from "@/infra/graphql/generated/graphql";
-import { Button, ButtonIcon } from "@/presentation/primitive/component/button/button.presenter";
 import { Icon } from "@/presentation/primitive/component/icon/icon.presenter";
 import twMerge from "@/presentation/style/twmerge";
 
@@ -17,27 +15,28 @@ const Ranking: FC<{ data: RankingType }> = ({ data }) => {
 	return (
 		<div className="mx-[15px] mt-[25px] w-[90%]">
 			<div className="rounded-3xl bg-white px-[20px] pt-[15px] pb-[5px] shadow-xl">
-				<div className="">
-					<div className="text-center font-body text-[40px] font-bold">あなたの順位</div>
-					<div className="text-center text-sm ">全参加者128人の中で...</div>
-					<div className="mb-[50px] flex translate-y-[-20px] justify-center">
-						<div className={twMerge("mr-[10px] translate-y-[55px] text-5xl", data.rank === 1 && "text-gold", data.rank === 2 && "text-silver", data.rank === 3 && "text-[#d8b041]")}>#{data.rank}</div>
-						<Icon src={`/icons/${data.character?.toLocaleLowerCase()}.png`} className="h-[10px] w-[20px] translate-y-10 p-10" />
+				<div>
+					<div className="flex translate-y-[-20px] justify-center">
+						<div>
+							<div className="text-center font-body text-[40px] font-bold">あなたの順位</div>
+							<div className="text-center text-sm ">全参加者128人の中で...</div>
+							<div className="mb-[50px] flex translate-y-[-20px] justify-center">
+								<div className={twMerge("mr-[10px] translate-y-[55px] text-5xl", data.rank === 1 && "text-gold", data.rank === 2 && "text-silver", data.rank === 3 && "text-[#d8b041]")}>#1</div>
+								<Icon src={`/icons/${data.character?.toLocaleLowerCase()}.png`} className="h-[10px] w-[20px] translate-y-10 p-10" />
+								<div className="ml-[10px] translate-y-[60px] text-center">
+									<div>57Ptをわが物とし栄冠を載するのは...</div>
+									<div className="font-body text-3xl font-bold">Dino316</div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
-				<div className="my-[10px] flex justify-center">
+				<div className="mb-[10px] flex translate-y-[-20px] justify-center">
 					<TwitterShareButton
 						url={shareUrl}
 						title={title}
 					>
-						<Button className="bg-gradient-to-r from-twitter-g1 to-twitter-g2">
-							<ButtonIcon>
-								<RiTwitterLine />
-							</ButtonIcon>
-							<i>
-								ツイートする
-							</i>
-						</Button>
+						<TwitterIcon className="h-[100px]" round />
 					</TwitterShareButton>
 				</div>
 			</div>
