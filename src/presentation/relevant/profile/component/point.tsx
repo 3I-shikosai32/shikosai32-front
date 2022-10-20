6ce4350c0c61579e46665f667e9463d655963c28
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-21 00:50:03 +0900

    feat: ✨ (new-profile) pointコンポーネントの作成

---
+++ src/presentation/relevant/profile/component/point.tsx
@@ -0,0 +1,27 @@
+import type { FC } from "react";
+import { BsFillGiftFill } from "react-icons/bs";
+import { Button, ButtonIcon } from "@/presentation/primitive/component/button/button.presenter";
+
+
+const Point: FC = () => (
+	<div className="mx-[10px] mt-[25px] w-[90%] rounded-3xl bg-white shadow-xl">
+		<div className="flex justify-center px-[20px] ">
+			<div>
+				<div className="text-center text-[40px] font-bold">残り消費Pt</div>
+				<div className="text-center text-3xl">
+					<span className="text-5xl">34</span>
+					<span>pt</span>
+				</div>
+			</div>
+		</div>
+		<div className="my-[10px] flex justify-center">
+			<Button className="bg-gradient-to-r from-[#E07272] to-[#FFB661]">
+				<ButtonIcon>
+					<BsFillGiftFill />
+				</ButtonIcon>
+				<i>景品を交換する</i>
+			</Button>
+		</div>
+	</div>
+)
+export default Point
