fb08af9e026f6bf69d9375b68816428e788045e7
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-21 00:50:29 +0900

    feat: ✨ (new-profile) rankingコンポーネントの作成

---
+++ src/presentation/relevant/profile/component/ranking.tsx
@@ -0,0 +1,58 @@
+import type { FC } from "react";
+import { useState, useEffect } from "react";
+import { RiTwitterLine } from "react-icons/ri";
+import { TwitterShareButton } from "react-share"
+import type { Character } from "@/infra/graphql/generated/graphql";
+import { Button, ButtonIcon } from "@/presentation/primitive/component/button/button.presenter";
+import { Icon } from "@/presentation/primitive/component/icon/icon.presenter";
+import twMerge from "@/presentation/style/twmerge";
+
+export type RankingType = {
+	name: string,
+	rank: number,
+	character: Character | null,
+	point: number,
+}
+const Ranking: FC<{ data: RankingType }> = ({ data }) => {
+	const [state, setState] = useState<RankingType>();
+	useEffect(() => {
+		setState({ name: data.name, rank: data.rank, character: data.character, point: data.point })
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
+	const shareUrl = 'http://github.com';
+	const title = 'GitHub';
+	return (
+		<div className="mx-[15px] mt-[25px] w-[90%]">
+			<div className="rounded-3xl bg-white px-[20px] pt-[15px] pb-[5px] shadow-xl">
+				<div className="">
+					<div className="text-center font-body text-[40px] font-bold">あなたの順位</div>
+					<div className="text-center text-sm ">全参加者128人の中で...</div>
+					<div className="mb-[50px] flex translate-y-[-20px] justify-center">
+						<div className={twMerge("mr-[10px] translate-y-[55px] text-5xl", data.rank === 1 && "text-gold", data.rank === 2 && "text-silver", data.rank === 3 && "text-[#d8b041]")}>#{data.rank}</div>
+						<Icon src={`/icons/${data.character?.toLocaleLowerCase()}.png`} className="h-[10px] w-[20px] translate-y-10 p-10" />
+						<div className="ml-[10px] translate-y-[60px] text-center">
+							<div>{state?.point}pt</div>
+							<div className="font-body text-3xl font-bold">{state?.name}</div>
+						</div>
+					</div>
+				</div>
+				<div className="my-[10px] flex justify-center">
+					<TwitterShareButton
+						url={shareUrl}
+						title={title}
+					>
+						<Button className="bg-gradient-to-r from-twitter-g1 to-twitter-g2">
+							<ButtonIcon>
+								<RiTwitterLine />
+							</ButtonIcon>
+							<i>
+								ツイートする
+							</i>
+						</Button>
+					</TwitterShareButton>
+				</div>
+			</div>
+		</div>
+	)
+}
+export default Ranking
