4e9376e0978db2bbe7d98ff52c7b777e01db0d37
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-20 22:55:49 +0900

    feat: ✨ (profile) indexにuidを取得する処理を追加

--- src/presentation/relevant/profile/index.tsx
+++ src/presentation/relevant/profile/index.tsx
@@ -1,19 +1,34 @@
 import type { FC } from 'react';
-import Character from './component/character';
+import CharacterComponets, { CharacterType } from './component/character';
 import Congrat from './component/congrat';
 import Item from './component/item'
 import Point from './component/point';
 import Ranking from './component/ranking';
+import useGetUser from './hooks/useGetUser';
+import { Character } from '@/infra/graphql/generated/graphql';
 import { Layout } from '@/presentation/layout/layout.container';
+import { useCurrentUserIdUseCase } from '@/use-case/user/use-current-user-id.use-case';
 
-const Profile: FC = () => (
-	<Layout>
-		<Character />
-		<Congrat />
-		<Ranking />
-		<Point />
-		<Item />
-	</Layout>
-)
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const character_state: CharacterType = {
+	name: "hoge",
+	character: Character.Cat,
+}
+
+const Profile: FC = () => {
+	const uid = useCurrentUserIdUseCase()
+	const { name, character, fetching, error } = useGetUser(uid);
+	return (
+		<Layout>
+			<CharacterComponets data={character_state}/>
+			<Congrat />
+			<Ranking />
+			<Point />
+			<Item />
+		</Layout>
+	)
+}
+
 
 export default Profile;
+
