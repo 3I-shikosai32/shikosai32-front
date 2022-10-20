00eab95c551bce1ac796fc57aefad3dce01a4e23
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-21 00:50:47 +0900

    feat: ✨ (new-profile) characterコンポーネントの作成

---
+++ src/presentation/relevant/profile/component/character.tsx
@@ -0,0 +1,31 @@
+import type { FC } from "react";
+import { twMerge } from "tailwind-merge";
+import { Character } from "@/infra/graphql/generated/graphql";
+import { Icon } from "@/presentation/primitive/component/icon/icon.presenter";
+
+export type CharacterType = {
+	name: string | null,
+	character: Character | null
+}
+
+const CharacterComponets: FC<{ data: CharacterType }> = ({ data }) =>
+(
+	<div className="w-full">
+		<div className={twMerge("flex justify-center bg-gradient-to-r",
+			data.character === Character.Cat && 'from-character-cat-g1 to-character-cat-g2',
+			data.character === Character.Fox && 'from-character-fox-g1 to-character-fox-g2',
+			data.character === Character.Goku && 'from-character-goku-g1 to-character-goku-g2',
+			data.character === Character.Pudding && 'from-character-pudding-g1 to-character-pudding-g2',
+			data.character === Character.Reaper && 'from-character-reaper-g1 to-character-reaper-g2',
+			data.character === Character.Tree && 'from-character-tree-g1 to-character-tree-g2',
+		)}>
+			<Icon src={`/icons/${data.character?.toLocaleLowerCase()}.png`} className="h-[150px] w-[150px] translate-y-10 p-10" />
+			<div className="ml-[20px] translate-y-[70px] text-6xl text-white">{data.name}</div>
+		</div>
+	</div>
+)
+
+
+
+
+export default CharacterComponets
