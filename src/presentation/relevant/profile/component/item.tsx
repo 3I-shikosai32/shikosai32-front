a7ce9886361e758478853b23082c6fb428fc5697
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-21 00:51:03 +0900

    feat: ✨ (new-profile) itemコンポーネントの作成

---
+++ src/presentation/relevant/profile/component/item.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import type { FC } from "react";
+
+export type ItemType = {
+	name: string
+}
+
+const Item: FC<{ data: ItemType }> = ({ data }) => (
+	<div className="mx-[10px] mt-[25px] mb-[50px] w-[90%]">
+		<div className="flex justify-center rounded-3xl bg-white px-[20px] shadow-xl">
+			<div>
+				<div className="text-center text-[40px] font-bold">アイテム獲得状況</div>
+				<div className="mt-[10px] text-center">あなたが持っているきゅうびさんおアイテムは</div>
+				<div className="mt-[30px] text-center font-pixel text-[30px] text-primary-700">4つ獲得済み!</div>
+				<div className="flex justify-center ">
+					<div className="mb-0 h-[200px] w-[250px] translate-y-[-20px]">
+						<Image src="/character/fox/base.png" width={250} height={200} alt="My icon" layout="responsive" />
+					</div>
+				</div>
+				<div className="translate-y-[-50px] text-center text-[30px] font-bold">
+					{data.name}
+				</div>
+			</div>
+		</div>
+	</div>
+)
+export default Item
