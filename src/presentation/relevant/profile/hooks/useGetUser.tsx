d8545fbf35b601a2c800b09337cc85087e30c770
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-20 22:54:50 +0900

    feat: ✨ (profile) useGetUserの追加

---
+++ src/presentation/relevant/profile/hooks/useGetUser.tsx
@@ -0,0 +1,23 @@
+import { useFindUserMetaDataQuery } from "@/infra/graphql/generated/graphql";
+import type { UseCurrentUserIdUseCaseResult } from "@/use-case/user/use-current-user-id.use-case";
+
+
+const useGetUser = (user: UseCurrentUserIdUseCaseResult) => {
+	const [result] = useFindUserMetaDataQuery({
+		variables: { id: user?.id || '**idが未指定のときはQueryは送られません**' },
+		pause: !user?.id,
+		requestPolicy: 'cache-and-network',
+	});
+	const { data, fetching, error } = result;
+	const name = data?.findUser?.name;
+	const character = data?.findUser?.characterStatus.character;
+	const url = data?.findUser?.characterStatus.iconUrl;
+	return {
+		name,
+		character,
+		url,
+		fetching,
+		error
+	}
+}
+export default useGetUser
