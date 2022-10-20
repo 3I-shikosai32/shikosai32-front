a5364907c390f55b8d69f6ab8b3efe93274b97e7
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-20 22:56:29 +0900

    feat: ✨ (profile) キャラクターごとに色を変更

--- src/presentation/relevant/profile/component/character.tsx
+++ src/presentation/relevant/profile/component/character.tsx
@@ -1,12 +1,19 @@
 import type { FC } from "react";
+import { twMerge } from "tailwind-merge";
+import type { Character } from "@/infra/graphql/generated/graphql";
 import { Icon } from "@/presentation/primitive/component/icon/icon.presenter";
 
-const Character: FC = () => (
+export type CharacterType = {
+	name: string | null,
+	character: Character | null
+}
+const CharacterComponets: FC<{ data: CharacterType }> = ({ data }) => (
 	<div className="w-full">
-		<div className="flex justify-center bg-gradient-to-r from-character-fox-g1 to-character-fox-g2">
-			<Icon src="/icons/cat.png" className="h-[150px] w-[150px] translate-y-10 p-10" />
-			<div className="ml-[20px] translate-y-[70px] text-6xl text-white">Dino3161</div>
+		<div className={twMerge("flex justify-center bg-gradient-to-r", `from-character-${data.character?.toLocaleLowerCase()}-g1`, `from-character-${data.character?.toLocaleLowerCase()}-g2`)}>
+			<Icon src={`/icons/${data.character?.toLocaleLowerCase()}.png`} className="h-[150px] w-[150px] translate-y-10 p-10" />
+			<div className="ml-[20px] translate-y-[70px] text-6xl text-white">{data.name}</div>
 		</div>
 	</div>
 )
-export default Character
+
+export default CharacterComponets
