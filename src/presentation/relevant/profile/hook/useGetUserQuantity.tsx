cbc9c0db98a8362db8ca91021bb2ba29412ab6ad
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-22 01:22:47 +0900

    fix: 🐛 (new-profile) セミコロンの抜けを修正

--- src/presentation/relevant/profile/hook/useGetUserQuantity.tsx
+++ src/presentation/relevant/profile/hook/useGetUserQuantity.tsx
@@ -1,7 +1,7 @@
-import { useFindAllUsersNameUseCase } from "@/use-case/user/use-find-all-users-name.use-case"
+import { useFindAllUsersNameUseCase } from '@/use-case/user/use-find-all-users-name.use-case';
 
 const useGetUserQuantity = (): number | undefined => {
 	const users = useFindAllUsersNameUseCase();
 	return users?.length;
 }
-export default useGetUserQuantity
+export default useGetUserQuantity;
