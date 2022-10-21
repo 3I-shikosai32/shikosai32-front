160547edd83b52463e0567af4357bf35b6b63e2c
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-21 15:28:46 +0900

    fix: 🐛 (new-profile) itemでサーバから取得したアイテム画像を表示するように変更

--- src/presentation/relevant/profile/component/item.tsx
+++ src/presentation/relevant/profile/component/item.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import type { FC } from "react";
+import type { GetObtainmentStatusesResult } from '../hooks/useGetObtainment'
 
 export type ItemType = {
 	name: string | undefined,
-	url: string | undefined
+	url: string | undefined,
+	quantity: number | undefined,
+	obtainmentStatuses: GetObtainmentStatusesResult,
+
 }
 
 const Item: FC<{ data: ItemType }> = ({ data }) => (
@@ -12,7 +16,7 @@ const Item: FC<{ data: ItemType }> = ({ data }) => (
 			<div>
 				<div className="text-center text-[40px] font-bold">アイテム獲得状況</div>
 				<div className="mt-[10px] text-center">あなたが持っているきゅうびさんおアイテムは</div>
-				<div className="mt-[30px] text-center font-pixel text-[30px] text-primary-700">4つ獲得済み!</div>
+				<div className="mt-[30px] text-center font-pixel text-[30px] text-primary-700">{data.quantity ? data.quantity : 0}つ獲得済み!</div>
 				<div className="flex justify-center ">
 					<div className="mb-0 h-[200px] w-[250px] translate-y-[-20px]">
 						<Image src={data.url ? data.url : ""} width={250} height={200} alt="My icon" layout="responsive" />
@@ -21,8 +25,19 @@ const Item: FC<{ data: ItemType }> = ({ data }) => (
 				<div className="translate-y-[-50px] text-center text-[30px] font-bold">
 					{data.name}
 				</div>
+				<div className="mb-[30px] grid grid-cols-4 gap-6">
+					{
+						data.obtainmentStatuses?.map((status, index) => (
+							// eslint-disable-next-line react/no-array-index-key
+							<div key={index}>
+								<Image src={status.iconUrl} width={100} height={100} alt="character item" layout="responsive" />
+							</div>
+						))
+					}
+				</div>
 			</div>
 		</div>
 	</div>
 )
+
 export default Item
