5cedaf6dae0e7ab4d2e1fd02685e0b39302330ae
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-22 01:23:28 +0900

    fix: 🐛 (new-profile) セミコロンの抜けを修正

--- src/presentation/relevant/profile/hook/useGetTotalPoint.tsx
+++ src/presentation/relevant/profile/hook/useGetTotalPoint.tsx
@@ -1,7 +1,7 @@
 import { isBefore } from 'date-fns';
 import { zonedTimeToUtc } from 'date-fns-tz';
-import { useGetTotalPointQuery } from "@/infra/graphql/generated/graphql";
-import type { UseCurrentUserIdUseCaseResult } from "@/use-case/user/use-current-user-id.use-case";
+import { useGetTotalPointQuery } from '@/infra/graphql/generated/graphql';
+import type { UseCurrentUserIdUseCaseResult } from '@/use-case/user/use-current-user-id.use-case';
 
 const useGetTotalPoint = (user: UseCurrentUserIdUseCaseResult) => {
 	const date = new Date();
@@ -9,7 +9,7 @@ const useGetTotalPoint = (user: UseCurrentUserIdUseCaseResult) => {
 	const [{ data }] = useGetTotalPointQuery({
 		variables: { where: { authId: user?.id || '**idが未指定のときはQueryは送られません**' } },
 		pause: !user?.id,
-		requestPolicy: 'cache-and-network',
+		requestPolicy: 'cache-and-network', 
 	});
 	const totalPointDay1 = data?.findUser?.totalPointDay1;
 	const totalPointDay2 = data?.findUser?.totalPointDay2;
@@ -19,4 +19,4 @@ const useGetTotalPoint = (user: UseCurrentUserIdUseCaseResult) => {
 	return totalPointDay2;
 }
 
-export default useGetTotalPoint
+export default useGetTotalPoint;
