4849cce880794699cbcdeb7dfe9ec92ffc7b6c28
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-21 11:07:20 +0900

    feat: ✨ (new-profile) useFindRankingの作成

---
+++ src/presentation/relevant/profile/hooks/useFindRanking.tsx
@@ -0,0 +1,16 @@
+import { useFindRankingPositionQuery } from "@/infra/graphql/generated/graphql";
+import type { UseCurrentUserIdUseCaseResult } from "@/use-case/user/use-current-user-id.use-case";
+
+const useFindRanking = (user: UseCurrentUserIdUseCaseResult) => {
+	const [result] = useFindRankingPositionQuery({
+		variables: { userId: user?.id || '**idが未指定のときはQueryは送られません**' },
+		pause: !user?.id,
+		requestPolicy: 'cache-and-network',
+	});
+	const { data } = result;
+	const rank = data?.getRankingPosition
+	return {
+		rank
+	}
+}
+export default useFindRanking
