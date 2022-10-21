7f391f1f77623a4660c4b594bb61e52b3d0500fe
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2022-10-22 01:24:11 +0900

    fix: 🐛 (new-profile) 交換ボタンをシェアボタンに対応させた

--- src/presentation/relevant/profile/component/point.tsx
+++ src/presentation/relevant/profile/component/point.tsx
@@ -1,32 +1,32 @@
-import { useRouter } from 'next/router'
+/* eslint-disable jsx-a11y/interactive-supports-focus */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { useRouter } from 'next/router';
 import type { FC } from "react";
-import { BsFillGiftFill } from "react-icons/bs";
-import { Button, ButtonIcon } from "@/presentation/primitive/component/button/button.presenter";
-
+import { BsFillGiftFill } from 'react-icons/bs';
 
 const Point: FC<{ point: number | null | undefined }> = ({ point }) => {
-	const router = useRouter()
+	const router = useRouter();
 	return (
-		<div className="mx-[10px] mt-[25px] w-[90%] rounded-3xl bg-white pt-[20px] shadow-xl">
-			<div className="flex justify-center px-[20px] ">
+		<div className='mx-[10px] mt-[25px] w-[90%] rounded-3xl bg-white pt-[20px] shadow-xl'>
+			<div className='flex justify-center px-[20px]'>
 				<div>
-					<div className="text-center text-[40px] font-bold">残り消費Pt</div>
-					<div className="text-center text-3xl">
-						<span className="text-5xl">{point || 0}</span>
+					<div className='text-center text-[40px] font-bold'>残り消費Pt</div>
+					<div className='text-center text-3xl'>
+						<span className='text-5xl'>{point || 0}pt</span>
 						<span>pt</span>
 					</div>
 				</div>
 			</div>
-			<div className="my-[10px] flex justify-center">
-				<Button className="bg-gradient-to-r from-[#E07272] to-[#FFB661]" onClick={() => router.push('/exchange')}>
-					<ButtonIcon>
-						<BsFillGiftFill />
-					</ButtonIcon>
-					<i>景品を交換する</i>
-				</Button>
+			<div className='my-[10px] flex justify-center'>
+				<div role='button' onClick={() => router.push("/exchange")} className='my-[20px] flex w-[200px] rounded-full bg-gradient-to-r from-[#E07272] to-[#FFB661] py-[7px] font-body text-[20px] font-bold text-white shadow-xl transition duration-300 hover:scale-105 active:scale-95'>
+					<div className='translate-x-[15px]'>
+						<BsFillGiftFill size={30} />
+					</div>
+					<div className='translate-x-[25px]'>景品交換する</div>
+				</div>
 			</div>
 		</div>
 	)
 }
 
-export default Point
+export default Point;
